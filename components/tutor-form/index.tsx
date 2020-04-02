f24bd6e410b7d0b759ae84b7284700cbe9269e85
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-01 17:13:01 -0700

    refactor: create src dir w/ packages and add deps

--- components/tutor-form/index.tsx
+++
@@ -1,9 +0,0 @@
-function TutorForm() {
-  return (
-    <div>
-      <p>This is the tutor form.</p>
-    </div>
-  );
-}
-
-export default TutorForm;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import Header from '../components/header';
-import Footer from '../components/footer';
-import PupilForm from '../components/pupil-form';
+import Header from '@tutorbook/covid-header';
+import Footer from '@tutorbook/covid-footer';
+import Form from '@tutorbook/covid-form';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header/>
-        <PupilForm />
+        <Form />
         <Footer />
       </div>
     );
