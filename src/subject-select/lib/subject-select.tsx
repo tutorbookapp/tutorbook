f68b2da17f5a71172b2fed7b87a91f9eace0dc2e
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-04 15:22:35 -0700

    refactor(subject-select): use conventional file structure

--- src/subject-select/lib/subject-select.tsx
+++ src/subject-select/lib/subject-select.tsx
@@ -1,6 +1,8 @@
 import { TextField } from '@rmwc/textfield'
 import { ChipSet, Chip } from '@rmwc/chip'
 
+import styles from './subject-select.module.scss'
+
 export default function SubjectSelect() {
   return (
     <TextField outlined>
