6e65ce2c8cc1d47198ff0d6e2c59798ba6c2fa4c
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-04 11:40:52 -0700

    wip(subject-select): add subject selection component

---
+++ src/subject-select/README.md
@@ -0,0 +1,11 @@
+# `@tutorbook/subject-select`
+
+> TODO: description
+
+## Usage
+
+```
+const subjectSelect = require('@tutorbook/subject-select');
+
+// TODO: DEMONSTRATE API
+```
---
+++ src/subject-select/lib/subject-select.tsx
@@ -0,0 +1,9 @@
+import { TextField } from '@rmwc/textfield'
+import { ChipSet, Chip } from '@rmwc/chip'
+
+export default function SubjectSelect() {
+  return (
+    <TextField outlined>
+    </TextField>
+  );
+}
