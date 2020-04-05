60519864929dc3d03c7d22701bc17c2258f7cbe3
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-04 19:37:01 -0700

    tests: fix jest config and typescript deps

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,29 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|s[ac]ss|less)$': 'identity-obj-proxy',
+  },
+  roots: [
+    '<rootDir>/src',
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        jsx: 'react',
+      },
+    },
+  },
 };
