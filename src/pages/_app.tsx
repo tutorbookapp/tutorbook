c1b38a1f4afcda30e6d882fcbaebffb54ebb9449
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-04 21:26:47 -0700

    fix: import Sass styling instead of dist CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,6 @@ import UserProvider from '../firebase/user'
 import '../styles'
 import '../styles/fonts.scss'
 
-import '@material/textfield/dist/mdc.textfield.css'
-import '@material/select/dist/mdc.select.css'
-import '@material/card/dist/mdc.card.css'
-import '@material/button/dist/mdc.button.css'
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <DBProvider>
