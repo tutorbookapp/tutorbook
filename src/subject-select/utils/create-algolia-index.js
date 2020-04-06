536f88b75f0961ed3729b4abbc5e1332e380dbb2
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-06 13:35:59 -0700

    chore(subject-select/utils): add subjects to algolia search index

--- src/subject-select/utils/create-algolia-index.js
+++ src/subject-select/utils/create-algolia-index.js
@@ -1,3 +1,22 @@
 const id = 'TODO: ADD-ALGOLIA-APP-ID-HERE';
 const key = 'TODO: ADD-ALGOLIA-KEY-HERE';
 const client = require('algoliasearch')(id, key);
+const to = require('await-to-js').default;
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
+
+const main = async () => {
+  const subjects = parse(fs.readFileSync('./subjects.csv'), {
+    columns: true,
+    skip_empty_lines: true,
+  });
+  const [err, res] = await to(client.initIndex('subjects').saveObjects(subjects, {
+    autoGenerateObjectIDIfNotExist: true,
+  }));
+  if (err) {
+    console.error('[ERROR] While saving subjects:', err);
+    debugger;
+  }
+};
+
+main();
