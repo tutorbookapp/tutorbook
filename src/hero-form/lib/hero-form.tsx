2cb6d57345ffb6b45afe28e9d26d2adaf88025fc
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-06 21:38:59 -0700

    feat(hero-form): add hero form component w/ background

--- src/hero-form/lib/hero-form.tsx
+++ src/hero-form/lib/hero-form.tsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import Form from '@tutorbook/covid-form'
 
+import styles from './hero-form.module.scss'
+
 export default function HeroForm() {
   return (
-    <Form 
-      inputs={[{
-        label: 'Subjects you want tutoring for',
-        el: 'subjectselect',
-        required: true,
-      }, {
-        label: 'When you want tutoring',
-        el: 'textfield',
-        required: true,
-      }]}
-      submitLabel='Request free tutoring'
-      onSubmit={(formValues) => new Promise((res, rej) => {})}
-    />
+    <>
+      <div className={styles.heroFormWrapper}>
+        <div className={styles.heroFormInnerWrapper}>
+          <Form 
+            inputs={[{
+              label: 'Your name',
+              el: 'textfield',
+              required: true,
+            }, {
+              label: 'Your email address',
+              el: 'textfield',
+              type: 'email',
+              required: true,
+            }, {
+              label: 'Subjects you want tutoring for',
+              el: 'subjectselect',
+              required: true,
+            }, {
+              label: 'When you want tutoring',
+              el: 'textfield',
+              required: true,
+            }]}
+            submitLabel='Request free tutoring'
+            onFormSubmit={(formValues) => new Promise((res, rej) => {})}
+            className={styles.heroForm}
+            cardProps={{
+              className: styles.heroFormCard,
+            }}
+          />
+        </div>
+      </div>
+      <div className={styles.heroBackground} />
+    </>
   );
 }
