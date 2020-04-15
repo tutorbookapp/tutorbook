5da25b003e18fccac6c25a4f365dc31c36d40418
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-15 09:55:15 -0700

    feat(tutor-form): form creates user model

--- src/tutor-form/lib/covid-tutor-form.tsx
+++ src/tutor-form/lib/covid-tutor-form.tsx
@@ -1,20 +1,18 @@
 import Form from '@tutorbook/covid-form';
 import { useDB } from '@tutorbook/next-firebase/db';
+import { Availability, UserInterface, userConverter } from '@tutorbook/model';
 
 import styles from './covid-tutor-form.module.scss';
 
 /**
  * React component that collects the following information from tutors and
  * create their Firestore user document:
- * - Email
- * - Name
- * - Phone
- * - Education (e.g. "in college", "college", "masters", "pHD")
- * - Do you have experience tutoring professionally?
- * - If so, how many years have you been tutoring?
- * - School (text field)
- * - Hobbies
- * - Message
+ * - (name) Your name
+ * - (email) Your email address
+ * - (phone?) Your phone number
+ * - (subjects) What can you tutor?
+ * - (availability) When can you tutor?
+ * - (bio) Education (e.g. "in college", "college", "masters") and experience
  */
 export default function TutorForm() {
   const db = useDB();
@@ -27,48 +25,56 @@ export default function TutorForm() {
               label: 'Your name',
               el: 'textfield',
               required: true,
+              key: 'name',
             },
             {
               label: 'Your email address',
               type: 'email',
               el: 'textfield',
               required: true,
+              key: 'email',
             },
             {
               label: 'Your phone number',
               type: 'tel',
               el: 'textfield',
+              key: 'phone',
             },
             {
-              label: 'Subjects you can tutor',
+              label: 'What can you tutor?',
               el: 'subjectselect',
               required: true,
+              key: 'subjects',
             },
             {
-              label: 'Availability for tutoring',
-              el: 'textfield',
+              label: 'When can you tutor?',
+              el: 'scheduleinput',
               required: true,
+              key: 'availability',
             },
             {
               label: 'Education and experience',
               el: 'textarea',
               required: true,
-            },
-            {
-              label: 'Message',
-              el: 'textarea',
+              key: 'bio',
             },
           ]}
-          title='Volunteer as a Tutor'
+          title='Become a Tutor'
           description={
-            'We are building a massive academic support network and systems to ' +
-            'bolster our educational infrastructure in this difficult time. If you ' +
-            'have expertise in marketing, management, teaching, tech, or just want ' +
-            'to help out we would love to have you!'
+            'We are building a massive academic support network and systems ' +
+            'to bolster our educational infrastructure in this difficult ' +
+            'time. If you have expertise in marketing, management, teaching, ' +
+            'tech, or just want to help out we would love to have you!'
           }
           submitLabel='Volunteer to tutor'
           onFormSubmit={(formValues) => {
-            return db.collection('tutors').doc().set(formValues);
+            const tutor: UserInterface = formValues as UserInterface;
+            tutor.schedule = new Availability();
+            return db
+              .collection('tutors')
+              .doc()
+              .withConverter(userConverter)
+              .set(tutor);
           }}
         />
       </div>
