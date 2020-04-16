60250ecfa3cc702b01e556adf17c71bb1abea9ef
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-15 19:35:24 -0700

    feat(firebase/functions): sync user data w/ algolia index

---
+++ firebase/functions/src/algolia.ts
@@ -0,0 +1,25 @@
+import { Change, EventContext } from 'firebase-functions';
+import { DocumentSnapshot } from '@google-cloud/firestore';
+import { SearchClient, SearchIndex } from 'algoliasearch';
+
+import * as functions from 'firebase-functions';
+import algoliasearch from 'algoliasearch';
+
+const client: SearchClient = algoliasearch(
+  functions.config().algolia.id,
+  functions.config().algolia.key
+);
+
+export async function userUpdate(
+  change: Change<DocumentSnapshot>,
+  context: EventContext
+): Promise<void> {
+  const index: SearchIndex = client.initIndex('users');
+  if (!change.after.exists || !change.after.data()) {
+    await index.deleteObject(context.params.user);
+  } else {
+    const ob: Record<string, any> = change.after.data() as Record<string, any>;
+    ob.objectID = context.params.user;
+    await index.saveObject(ob);
+  }
+}
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,1 +1,6 @@
 import * as functions from 'firebase-functions';
+import { userUpdate } from './algolia';
+
+export const algoliaUserUpdate = functions.firestore
+  .document('partitions/{partition}/users/{user}')
+  .onWrite(userUpdate);
