39e46744c44e8ae68604367b7e4f1b693449555d
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-18 16:39:14 -0700

    feat: move typography provider to top of app

--- src/hero-about/lib/hero-about.tsx
+++ src/hero-about/lib/hero-about.tsx
@@ -1,77 +1,62 @@
 import React from 'react';
 import Link from 'next/link';
 import Banner from '@tutorbook/banner';
-import { RMWCProvider } from '@rmwc/provider';
 import { Typography } from '@rmwc/typography';
 
 import styles from './hero-about.module.scss';
 
 export default function HeroAbout() {
   return (
-    <RMWCProvider
-      typography={{
-        defaultTag: 'div',
-        headline1: 'h1',
-        headline2: 'h2',
-        headline3: 'h3',
-        headline4: 'h4',
-        headline5: 'h5',
-        headline6: 'h6',
-        body1: 'p',
-        body2: 'p',
-      }}
-    >
-      <div className={styles.aboutWrapper}>
-        <div className={styles.aboutContent}>
-          <Typography use='headline1'>About us</Typography>
+    <div className={styles.aboutWrapper}>
+      <div className={styles.aboutContent}>
+        <Typography use='headline1'>About us</Typography>
+        <Typography use='body1'>
+          <b>Tutorbook is a free, online tutoring platform</b> that connects
+          students affected by school closures with volunteer tutors.
+        </Typography>
+        <Typography use='body1'>
+          In doing so, we are making universal, free, and online education a
+          reality in this time of school crises. Here’s how you can help:
+        </Typography>
+        <ul style={{ listStyle: 'decimal' }}>
+          <li>Inform every parent and high school student that you know!</li>
+          <li>
+            Re-post this message and promote us so we can help more students!
+          </li>
+          <li>
+            Help us recruit more volunteers by following #2 and by signing up{' '}
+            <Link href='/tutors'>
+              <a>here</a>
+            </Link>
+            .
+          </li>
+        </ul>
+        <Banner>
+          <Typography use='headline2'>Our mission</Typography>
           <Typography use='body1'>
-            <b>Tutorbook is a free, online tutoring platform</b> that connects
-            students affected by school closures with volunteer tutors.
+            Students across the globe no longer have face-to-face support from
+            teachers or in-person collaboration due to COVID-19.
           </Typography>
           <Typography use='body1'>
-            In doing so, we are making universal, free, and online education a
-            reality in this time of school crises. Here’s how you can help:
+            In response, we have reached out to university students and
+            educators from around the world—people who want to make a difference
+            but don’t know how.
           </Typography>
-          <ul style={{ listStyle: 'decimal' }}>
-            <li>Inform every parent and high school student that you know!</li>
-            <li>
-              Re-post this message and promote us so we can help more students!
-            </li>
-            <li>
-              Help us recruit more volunteers by following #2 and by signing up{' '}
-              <Link href='/tutors'>
-                <a>here</a>
-              </Link>
-              .
-            </li>
-          </ul>
-          <Banner>
-            <Typography use='headline2'>Our mission</Typography>
-            <Typography use='body1'>
-              Students across the globe no longer have face-to-face support from
-              teachers or in-person collaboration due to COVID-19.
-            </Typography>
-            <Typography use='body1'>
-              In response, we have reached out to university students and
-              educators from around the world—people who want to make a
-              difference but don’t know how.
-            </Typography>
-            <Typography use='body1'>
-              As a result of the kindness and generosity of these talented
-              individuals, students now have access to free one-on-one academic
-              mentorship through our platform (and highly talented individuals
-              have a means to make a difference from home 😉).
-            </Typography>
-            <Typography use='body1'>
-              Want to help out? Volunteer{' '}
-              <Link href='/tutors'>
-                <a>here</a>
-              </Link>
-              .
-            </Typography>
-          </Banner>
-        </div>
+          <Typography use='body1'>
+            As a result of the kindness and generosity of these talented
+            individuals, students now have access to free one-on-one academic
+            mentorship through our platform (and highly talented individuals
+            have a means to make a difference from home 😉).
+          </Typography>
+          <Typography use='body1'>
+            Want to help out? Volunteer{' '}
+            <Link href='/tutors'>
+              <a>here</a>
+            </Link>
+            .
+          </Typography>
+        </Banner>
       </div>
-    </RMWCProvider>
+    </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import { RMWCProvider } from '@rmwc/provider';
 
 import DBProvider from '../firebase/db';
 
@@ -6,8 +7,22 @@ import '../styles';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DBProvider>
-      <Component {...pageProps} />
-    </DBProvider>
+    <RMWCProvider
+      typography={{
+        defaultTag: 'div',
+        headline1: 'h1',
+        headline2: 'h2',
+        headline3: 'h3',
+        headline4: 'h4',
+        headline5: 'h5',
+        headline6: 'h6',
+        body1: 'p',
+        body2: 'p',
+      }}
+    >
+      <DBProvider>
+        <Component {...pageProps} />
+      </DBProvider>
+    </RMWCProvider>
   );
 }
