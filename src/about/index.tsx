86ba1ac625ff6fb7a9ec53066bacfa1b1a1874db
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-18 08:53:35 -0700

    chore: update homepage url

--- src/about/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.scss';
-
-export default function About() {
-  return (
-    <div className={styles.aboutWrapper}>
-      <p>This is the about page content.</p>
-    </div>
-  );
-}
