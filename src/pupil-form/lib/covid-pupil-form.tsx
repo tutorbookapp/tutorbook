164c8a1b68096387038da4f4fdb44e741f7fa903
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-21 13:59:00 -0700

    wip(pupil-form): group grade options by school type
    Fixes #14 with a kind of messy workaround. See the original issue for
    more info: https://github.com/jamesmfriedman/rmwc/issues/614

--- src/pupil-form/lib/covid-pupil-form.tsx
+++ src/pupil-form/lib/covid-pupil-form.tsx
@@ -1,25 +1,44 @@
 import Router from 'next/router';
+import { FormattedOption } from '@rmwc/select';
 import Form, { InputElAlias } from '@tutorbook/covid-form';
 import { User } from '@tutorbook/model';
 import { UserProvider } from '@tutorbook/next-firebase';
 
 import styles from './covid-pupil-form.module.scss';
 
-// TODO: Remove this and support multiple languages and ed systems.
-const GRADES: string[] = [
-  'Senior',
-  'Junior',
-  'Sophomore',
-  'Freshman',
-  '8th Grade',
-  '7th Grade',
-  '6th Grade',
-  '5th Grade',
-  '4th Grade',
-  '3rd Grade',
-  '2nd Grade',
-  '1st Grade',
-  'Kindergarten',
+/**
+ * This object represents the options that the user can select for 'grade'. Note
+ * that we're using a workaround to make them separate list groups (but this
+ * workaround **still** results in the creation of `mdc-list-group__subheaders`
+ * that we don't want).
+ * @todo Remove this altogether and support multiple languages and ed systems.
+ * @todo Remove the `mdc-list-group__subheaders` from the resulting `Menu`.
+ * @see {@link https://github.com/jamesmfriedman/rmwc/issues/614}
+ */
+const GRADES: FormattedOption[] = [
+  {
+    label: null,
+    options: ['Senior', 'Junior', 'Sophomore', 'Freshman'].map((label) => ({
+      label,
+    })),
+  },
+  {
+    label: null,
+    options: ['8th Grade', '7th Grade', '6th Grade'].map((label) => ({
+      label,
+    })),
+  },
+  {
+    label: null,
+    options: [
+      '5th Grade',
+      '4th Grade',
+      '3rd Grade',
+      '2nd Grade',
+      '1st Grade',
+      'Kindergarten',
+    ].map((label) => ({ label })),
+  },
 ];
 
 /**
