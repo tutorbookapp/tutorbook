90b80e35cb77b6a8a15c79ff74a25fd01c7d042d
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-21 15:20:29 -0700

    chore(src/pages/api/log*): remove unused api endpoints

--- src/pages/api/logout.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function logout(
-  req: NextApiRequest,
-  res: NextApiResponse<void>
-): void {
-  req.session.decodedToken = null;
-  res.status(200);
-}
