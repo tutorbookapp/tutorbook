6b8f79cbbbc729d984704398fa1421769187e2c4
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-05-15 09:10:40 -0700

    fix(emails/lib/email.ts): remove required 'recipient' prop

--- src/emails/lib/email.ts
+++ src/emails/lib/email.ts
@@ -1,7 +1,5 @@
 import { MailData } from '@sendgrid/helpers/classes/mail';
-import { User } from '@tutorbook/model';
 
 export default interface Email extends MailData {
-  readonly recipient: User;
   readonly html: string;
 }
