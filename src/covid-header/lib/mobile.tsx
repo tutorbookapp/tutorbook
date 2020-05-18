30530d1c53c32a855a56865cf891a231a7b20783
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-05-18 10:32:38 -0700

    fix(covid-header/lib/mobile): avoid FOUC by removing transition for first render

--- src/covid-header/lib/mobile.tsx
+++ src/covid-header/lib/mobile.tsx
@@ -10,6 +10,14 @@ import styles from './mobile.module.scss';
 
 export default function Nav(props: NavProps): JSX.Element {
   const [open, setOpen] = React.useState(false);
+  const [closing, setClosing] = React.useState(false);
+  const toggl = () => {
+    if (open) {
+      setClosing(true);
+      setTimeout(() => setClosing(false), 500);
+    }
+    setOpen(!open);
+  };
   return (
     <>
       <div className={styles.contentWrapper}>
@@ -24,7 +32,7 @@ export default function Nav(props: NavProps): JSX.Element {
         <button
           id='open-menu-btn'
           type='button'
-          onClick={() => setOpen(!open)}
+          onClick={toggl}
           className={
             styles.menuButtonWrapper +
             (open ? ' ' + styles.menuButtonWrapperActive : '')
@@ -51,43 +59,51 @@ export default function Nav(props: NavProps): JSX.Element {
         </button>
       </div>
       <div
-        className={styles.menu + (open ? ' ' + styles.menuActive : '')}
-        aria-hidden='true'
-        role='menu'
+        className={
+          styles.menuWrapper + (open || closing ? ' ' + styles.menuActive : '')
+        }
       >
-        <div className={styles.contentWrapper}>
-          <Link href='/'>
-            <a aria-label='Tutorbook Home'>
-              <img className={styles.logoImg} src={BlackLogo} />
-            </a>
-          </Link>
-          <button
-            id='open-menu-btn'
-            type='button'
-            onClick={() => setOpen(!open)}
-            className={
-              styles.menuButtonWrapper +
-              (open ? ' ' + styles.menuButtonWrapperActive : '')
-            }
-            aria-label='Open and close navigation menu'
-            aria-haspopup='true'
-          >
-            <span className={styles.menuLine + ' ' + styles.menuLineTop}></span>
-            <span
-              className={styles.menuLine + ' ' + styles.menuLineBottom}
-            ></span>
-          </button>
+        <div
+          className={styles.menu + (open ? ' ' + styles.menuActive : '')}
+          aria-hidden='true'
+          role='menu'
+        >
+          <div className={styles.contentWrapper}>
+            <Link href='/'>
+              <a aria-label='Tutorbook Home'>
+                <img className={styles.logoImg} src={BlackLogo} />
+              </a>
+            </Link>
+            <button
+              id='open-menu-btn'
+              type='button'
+              onClick={toggl}
+              className={
+                styles.menuButtonWrapper +
+                (open ? ' ' + styles.menuButtonWrapperActive : '')
+              }
+              aria-label='Open and close navigation menu'
+              aria-haspopup='true'
+            >
+              <span
+                className={styles.menuLine + ' ' + styles.menuLineTop}
+              ></span>
+              <span
+                className={styles.menuLine + ' ' + styles.menuLineBottom}
+              ></span>
+            </button>
+          </div>
+          <ul className={styles.categoryWrapper}>
+            {props.links.map((link: LinkProps, index: number) => (
+              <NavItem
+                {...link}
+                key={index}
+                menuItemClassName={styles.menuItem}
+                menuItemLinkClassName={styles.menuItemLink}
+              />
+            ))}
+          </ul>
         </div>
-        <ul className={styles.categoryWrapper}>
-          {props.links.map((link: LinkProps, index: number) => (
-            <NavItem
-              {...link}
-              key={index}
-              menuItemClassName={styles.menuItem}
-              menuItemLinkClassName={styles.menuItemLink}
-            />
-          ))}
-        </ul>
       </div>
     </>
   );
