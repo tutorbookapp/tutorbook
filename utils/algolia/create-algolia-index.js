33e3c77fb270b1a30095851a41db733b0cbd32fc
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-05-21 19:33:59 -0700

    feat(utils/algolia): add expertise search index

--- utils/algolia/create-algolia-index.js
+++ utils/algolia/create-algolia-index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env.prod') });
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const client = require('algoliasearch')(
   process.env.ALGOLIA_SEARCH_ID,
@@ -10,17 +10,17 @@ const to = require('await-to-js').default;
 const parse = require('csv-parse/lib/sync');
 const fs = require('fs');
 
-const main = async () => {
-  const index = client.initIndex('subjects');
+const main = async (id = 'subjects') => {
+  const index = client.initIndex(id);
   index.setSettings({
     attributesForFaceting: ['filterOnly(grades)', 'filterOnly(name)'],
   });
-  const subjects = parse(fs.readFileSync('./subjects.csv'), {
+  const subjects = parse(fs.readFileSync(`./${id}.csv`), {
     columns: true,
     skip_empty_lines: true,
   })
     .map((subject) => {
-      subject.grades = subject.grades.split(', ');
+      subject.grades = (subject.grades || '').split(', ');
       return subject;
     })
     .filter((subject) => !!subject.name);
@@ -30,9 +30,9 @@ const main = async () => {
     })
   );
   if (err) {
-    console.error('[ERROR] While saving subjects:', err);
+    console.error(`[ERROR] While saving ${id}:`, err);
     debugger;
   }
 };
 
-main();
+main('expertise');
