608524c6af0ae443f3e1f136f4988fa418afc81c
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-05-29 12:07:39 -0700

    fix(header): add mobile menu support

--- src/header/lib/header.tsx
+++ src/header/lib/header.tsx
@@ -11,67 +11,75 @@ interface HeaderProps {
 }
 
 export default function Header({ aspect, onChange }: HeaderProps): JSX.Element {
+  const [active, setActive] = React.useState<boolean>(false);
+  const toggleMobileMenu = () => {
+    const menuActive: boolean = !active;
+    if (menuActive) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+    setActive(menuActive);
+  };
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
-        <aside className={styles.upper}>
+        <div className={styles.left}>
           <Link href='/'>
             <div className={styles.logo}>
               <span>TB</span>
             </div>
           </Link>
-          <div className={styles.mobile}>
-            <div className={styles.toggle}></div>
-            <nav className={styles.mobileWrapper}>
-              <ul>
-                <a href='/volunteers'>
-                  <li>Mentors</li>
-                </a>
-                <a href='/volunteers'>
-                  <li>Tutors</li>
-                </a>
-                <a href='/pupils'>
-                  <li>Students</li>
-                </a>
-              </ul>
-            </nav>
+          <div
+            className={styles.tabs + (active ? ' ' + styles.tabsHidden : '')}
+          >
+            <a
+              className={
+                styles.tab + (aspect === 'mentoring' ? ' ' + styles.active : '')
+              }
+              onClick={() => onChange('mentoring')}
+            >
+              Mentoring
+            </a>
+            <a
+              className={
+                styles.tab + (aspect === 'tutoring' ? ' ' + styles.active : '')
+              }
+              onClick={() => onChange('tutoring')}
+            >
+              Tutoring
+            </a>
           </div>
-        </aside>
-        <aside className={styles.lower}>
-          <div className={styles.desktop}>
-            <div className={styles.left}>
-              <a
-                className={
-                  styles.link +
-                  (aspect === 'mentoring' ? ' ' + styles.active : '')
-                }
-                onClick={() => onChange('mentoring')}
-              >
-                Mentoring
-              </a>
-              <a
-                className={
-                  styles.link +
-                  (aspect === 'tutoring' ? ' ' + styles.active : '')
-                }
-                onClick={() => onChange('tutoring')}
-              >
-                Tutoring
-              </a>
-            </div>
-            <div className={styles.right}>
-              <a className={styles.link} href='/volunteers'>
-                Mentors
-              </a>
-              <a className={styles.link} href='/volunteers'>
-                Tutors
-              </a>
-              <a className={styles.link} href='/pupils'>
-                Students
-              </a>
-            </div>
-          </div>
-        </aside>
+        </div>
+        <div className={styles.mobileToggle} onClick={toggleMobileMenu}>
+          <div
+            className={
+              styles.toggle + (active ? ' ' + styles.toggleActive : '')
+            }
+          />
+        </div>
+        <nav
+          className={
+            styles.mobileNav + (active ? ' ' + styles.mobileNavActive : '')
+          }
+        >
+          <ul className={styles.mobileLinks}>
+            <a className={styles.mobileLink} href='/volunteers'>
+              <li className={styles.mobileLinkItem}>Become a mentor</li>
+            </a>
+            <a className={styles.mobileLink} href='/volunteers'>
+              <li className={styles.mobileLinkItem}>Become a tutor</li>
+            </a>
+          </ul>
+        </nav>
+        <div className={styles.desktopLinks}>
+          <a className={styles.desktopLink} href='/volunteers'>
+            Become a mentor
+          </a>
+          <a className={styles.desktopLink} href='/volunteers'>
+            Become a tutor
+          </a>
+        </div>
       </header>
     </div>
   );
