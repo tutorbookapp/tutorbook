d86cee6a5e6d4be2222cec6bb331986984d339b8
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-05-29 16:26:42 -0700

    wip(query-form): add flexibility to required props

--- src/query-form/lib/query-form.tsx
+++ src/query-form/lib/query-form.tsx
@@ -1,41 +1,59 @@
-import { Query } from '@tutorbook/model';
+import { Availability, Aspect, Query } from '@tutorbook/model';
 
 import React from 'react';
 import TutorsForm from './tutors-form';
 import MentorsForm from './mentors-form';
 
 interface QueryFormProps {
-  query: Query;
-  onChange: (query: Query) => any;
+  query?: Query;
+  aspect?: Aspect;
+  onChange?: (query: Query) => any;
   onSubmit?: (query: Query) => any;
 }
 
 export default function QueryForm({
   query,
+  aspect,
   onChange,
   onSubmit,
 }: QueryFormProps): JSX.Element {
+  const [qry, setQuery] = React.useState<Query>(
+    query || {
+      aspect: aspect || 'mentoring',
+      subjects: [],
+      availability: new Availability(),
+    }
+  );
+
+  if (query && query !== qry) setQuery(query);
+  if (aspect && aspect !== qry.aspect) setQuery({ ...qry, aspect });
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     event.stopPropagation();
-    if (onSubmit) onSubmit(query);
+    if (onSubmit) onSubmit(qry);
     return false;
   };
+  const handleChange = (query: Query) => {
+    setQuery(query);
+    if (onChange) onChange(query);
+  };
+
   return (
     <>
       <TutorsForm
-        query={query}
+        query={qry}
         button={!!onSubmit}
-        visible={query.aspect === 'tutoring'}
+        visible={qry.aspect === 'tutoring'}
         onSubmit={handleSubmit}
-        onChange={onChange}
+        onChange={handleChange}
       />
       <MentorsForm
-        query={query}
+        query={qry}
         button={!!onSubmit}
-        visible={query.aspect === 'mentoring'}
+        visible={qry.aspect === 'mentoring'}
         onSubmit={handleSubmit}
-        onChange={onChange}
+        onChange={handleChange}
       />
     </>
   );
