35083a41477a547a24344d3ac4dba93212833dca
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-05-31 09:05:58 -0700

    feat(avatar): add loading state flasher

--- src/avatar/index.tsx
+++ src/avatar/index.tsx
@@ -1,16 +1,21 @@
 import styles from './avatar.module.scss';
 
+interface AvatarProps {
+  src?: string;
+  loading?: boolean;
+}
+
 /**
  * This `Avatar` component will fill 100% of the width or height (which ever is
  * smaller) while maintaining a 1:1 aspect ratio. It will then scale, center and
  * crop the given image to fit that 1:1 area. If no image is given, the `Avatar`
  * is rendered as a gray square with the text 'No Photo' centered within.
  */
-export default function Avatar({ src }: { src?: string }): JSX.Element {
+export default function Avatar({ src, loading }: AvatarProps): JSX.Element {
   return (
-    <div className={styles.wrapper}>
-      <img className={styles.img} src={src} />
-      {!src && <div className={styles.noImg}>No Photo</div>}
+    <div className={styles.wrapper + (loading ? ' ' + styles.loading : '')}>
+      {!loading && <img className={styles.img} src={src} />}
+      {!src && !loading && <div className={styles.noImg}>No Photo</div>}
     </div>
   );
 }
