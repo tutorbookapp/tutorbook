e74250799af528c666de1c1bc26cefb5c6246dd6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-06-01 10:51:43 -0700

    fix(title): update background clipping

--- src/title/index.tsx
+++ src/title/index.tsx
@@ -1,9 +1,5 @@
 import styles from './title.module.scss';
 
 export default function Title({ children }: { children: string }): JSX.Element {
-  return (
-    <div className={styles.background}>
-      <h1 className={styles.title}>{children}</h1>
-    </div>
-  );
+  return <h1 className={styles.title}>{children}</h1>;
 }
