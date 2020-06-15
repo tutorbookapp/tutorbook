17a7baef643c1679a53d33bc9de9077faf02ec83
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-06-15 13:17:24 -0700

    docs(pages/dashboard): add doc comment on SSG

--- src/pages/[locale]/dashboard/index.tsx
+++ src/pages/[locale]/dashboard/index.tsx
@@ -19,6 +19,26 @@ import Footer from '@tutorbook/footer';
 
 import msgs from '@tutorbook/dashboard/msgs';
 
+/**
+ * Ideally, we'd use Next.js's automatic static optimization to pre-render a
+ * skeleton screen and then grab the user-specific data using SWR once Next.js
+ * hydrates the page client-side (with the `org` query).
+ *
+ * But, we can't do this right now due to the way our localization is setup.
+ * We can't use `getStaticPaths` for a subset of the dynamic paths (i.e. we have
+ * to provide both the `org` and the `locale` paths). So we'd be forced to
+ * render a skeleton screen in the default locale and then manually fetch the
+ * needed translations client-side once Next.js hydrates the page (and we know
+ * the `locale` query).
+ *
+ * So, we're just going to modify the desired specifications to have the various
+ * dashboard screens in the same `/[locale]/dashboard/` route--regardless of
+ * which account the dashboard is viewing (that will be managed by our
+ * `AccountProvider`).
+ *
+ * @see {@link https://github.com/vinissimus/next-translate/issues/129}
+ * @see {@link https://github.com/vercel/next.js/issues/14200}
+ */
 function OverviewDashboardPage(): JSX.Element {
   const { account } = useAccount();
   const intl: IntlShape = useIntl();
