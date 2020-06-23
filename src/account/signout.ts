c1b6282c33dbd9c2feb6f90d22e74c5f2fc049fa
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-06-23 15:10:07 -0700

    fix(account/signout): properly update local account

--- src/account/signout.ts
+++ src/account/signout.ts
@@ -1,9 +1,10 @@
 import firebase from '@tutorbook/firebase';
 import 'firebase/auth';
 
+import { User } from '@tutorbook/model';
 import { mutate } from 'swr';
 
 export default async function signout(): Promise<void> {
   await firebase.auth().signOut();
-  await mutate('/api/account', null);
+  await mutate('/api/account', new User().toJSON(), false);
 }
