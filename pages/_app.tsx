40c6ad98f0a929786979d6172acf1a249d00608b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-07-27 13:36:14 -0700

    chore: remove unused 'RMWCProvider'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app';
-import { RMWCProvider } from '@rmwc/provider';
 import { SWRConfig } from 'swr';
 import { ApiError } from 'lib/model';
 import { UserProvider } from 'lib/account';
@@ -61,23 +60,9 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <SWRConfig value={{ fetcher }}>
       <UserProvider>
-        <RMWCProvider
-          typography={{
-            defaultTag: 'div',
-            headline1: 'h1',
-            headline2: 'h2',
-            headline3: 'h3',
-            headline4: 'h4',
-            headline5: 'h5',
-            headline6: 'h6',
-            body1: 'p',
-            body2: 'p',
-          }}
-        >
-          <div id='portal' />
-          <CovidHead />
-          <Component {...pageProps} />
-        </RMWCProvider>
+        <div id='portal' />
+        <CovidHead />
+        <Component {...pageProps} />
       </UserProvider>
     </SWRConfig>
   );
