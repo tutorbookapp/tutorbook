2218db109be37451e01a6f653cee19e425044f6a
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-07-30 11:03:08 -0700

    fix: remove old timeslot model and input

--- lib/model/timeslot.ts
+++ lib/model/timeslot.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 
 import { RRule } from 'rrule';
-import { TimeUtils } from 'lib/utils';
 import { DAYS } from './constants';
 
 /**
@@ -98,6 +97,7 @@ export class Timeslot implements TimeslotBase<Date> {
    * of the other timeslot is equal to or after the starting time of this
    * timeslot **and** the ending time of the other timeslot is equal to or
    * before the ending time of this timeslot).
+   * @todo Check the `recur` value as well.
    */
   public contains(other: Timeslot): boolean {
     return (
@@ -131,77 +131,11 @@ export class Timeslot implements TimeslotBase<Date> {
     return str;
   }
 
-  /**
-   * Parses an input (see below for examples) into a `Timeslot`:
-   * > Mondays at 3:00 PM to 4:00 PM.
-   * > Monday at 3:00 PM to 3:30 PM.
-   * This getter should only ever be called within a `try{} catch {}` sequence
-   * b/c it will throw an error every time if `this.state.value` isn't parsable.
-   * @deprecated We're going to put these into strings within the React tree so
-   * that we can use `react-intl` for better i18n support (e.g. we'll set the
-   * localization in the `pages/_app.tsx` top-level component and all children
-   * components will render their `Date`s properly for that locale).
-   */
-  public static fromString(timeslot: string): Timeslot {
-    const split: string[] = timeslot.split(' ');
-    if (split.length !== 7) throw new Error('Invalid time string.');
-
-    const dayStr: string = split[0];
-    const fromStr: string = split[2];
-    const fromAMPM: string = split[3];
-    const toStr: string = split[5];
-    const toAMPM: string = split[6];
-
-    const day: keyof typeof Day = (dayStr.endsWith('s')
-      ? dayStr.slice(0, -1)
-      : dayStr) as keyof typeof Day;
-    const dayNum: DayAlias = Day[day];
-
-    let fromHr: number = Number(fromStr.split(':')[0]).valueOf();
-    const fromMin: number = Number(fromStr.split(':')[1]).valueOf();
-    if (fromAMPM === 'PM') {
-      fromHr += 12;
-    } else if (fromAMPM !== 'AM') {
-      throw new Error('Invalid AM/PM format for from time.');
-    }
-
-    let toHr: number = Number(toStr.split(':')[0]).valueOf();
-    const toMin: number = Number(toStr.split(':')[1]).valueOf();
-    if (toAMPM === 'PM' || toAMPM === 'PM.') {
-      toHr += 12;
-    } else if (toAMPM !== 'AM' && toAMPM !== 'AM.') {
-      throw new Error('Invalid AM/PM format for to time.');
-    }
-
-    return new Timeslot(
-      TimeUtils.getDate(dayNum, fromHr, fromMin),
-      TimeUtils.getDate(dayNum, toHr, toMin)
-    );
-  }
-
-  /**
-   * Helper string conversion method that's **only** used by the `TimeslotInput`
-   * to convert it's value into a timestring.
-   * @todo Move the parsing logic from that input to this class.
-   * @todo Merge this with the `toString()` method.
-   * @todo Don't assume that the `from` and `to` times occur on the same day.
-   * @deprecated We're going to put these into strings within the React tree so
-   * that we can use `react-intl` for better i18n support (e.g. we'll set the
-   * localization in the `pages/_app.tsx` top-level component and all children
-   * components will render their `Date`s properly for that locale).
-   */
-  public toParsableString(): string {
-    return (
-      `${DAYS[this.from.getDay()]}s from ${this.from.getHours()}:` +
-      `${`0${this.from.getMinutes()}`.slice(-2)} AM to ` +
-      `${this.to.getHours()}:${`0${this.to.getMinutes()}`.slice(-2)} AM`
-    );
-  }
-
   public equalTo(timeslot: TimeslotInterface): boolean {
     return (
-      timeslot.from.valueOf() === this.from.valueOf() &&
-      timeslot.to.valueOf() === this.to.valueOf()
+      timeslot.recur === this.recur &&
+      timeslot.to.valueOf() === this.to.valueOf() &&
+      timeslot.from.valueOf() === this.from.valueOf()
     );
   }
 
@@ -216,9 +150,11 @@ export class Timeslot implements TimeslotBase<Date> {
    * converted into the *correct* `Timestamp` type by the Firebase SDK itself).
    */
   public toFirestore(): TimeslotFirestore {
+    const { from, to, ...rest } = this;
     return {
-      from: (this.from as unknown) as Timestamp,
-      to: (this.to as unknown) as Timestamp,
+      ...rest,
+      from: (from as unknown) as Timestamp,
+      to: (to as unknown) as Timestamp,
     };
   }
 
@@ -226,19 +162,20 @@ export class Timeslot implements TimeslotBase<Date> {
    * Takes in a Firestore timeslot record and returns a new `Timeslot` object.
    */
   public static fromFirestore(data: TimeslotFirestore): Timeslot {
-    return new Timeslot(data.from.toDate(), data.to.toDate());
+    return new Timeslot(data.from.toDate(), data.to.toDate(), data.recur);
   }
 
   public toJSON(): TimeslotJSON {
-    return { from: this.from.toJSON(), to: this.to.toJSON() };
+    const { from, to, ...rest } = this;
+    return { ...rest, from: from.toJSON(), to: to.toJSON() };
   }
 
   public static fromJSON(json: TimeslotJSON): Timeslot {
-    return new Timeslot(new Date(json.from), new Date(json.to));
+    return new Timeslot(new Date(json.from), new Date(json.to), json.recur);
   }
 
   public static fromSearchHit(hit: TimeslotSearchHit): Timeslot {
-    return new Timeslot(new Date(hit.from), new Date(hit.to));
+    return new Timeslot(new Date(hit.from), new Date(hit.to), hit.recur);
   }
 
   public toURLParam(): string {
@@ -249,7 +186,8 @@ export class Timeslot implements TimeslotBase<Date> {
     const params: URLSearchParams = new URLSearchParams(param);
     return new Timeslot(
       new Date(params.get('from') as string),
-      new Date(params.get('to') as string)
+      new Date(params.get('to') as string),
+      params.get('recur') || undefined
     );
   }
 }
