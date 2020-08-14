68e1bae0517851468a0f326be5fa7d0ed2cbe79b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-08-14 16:22:58 -0600

    feat(components/people): add query inputs to filter sheet

--- components/people/filters-sheet.tsx
+++ components/people/filters-sheet.tsx
@@ -2,6 +2,8 @@ import { animated, useSpring } from 'react-spring';
 import { ResizeObserver as polyfill } from '@juggle/resize-observer';
 import useMeasure from 'react-use-measure';
 
+import { QueryInputs } from 'components/inputs';
+
 import { Callback, UsersQuery } from 'lib/model';
 
 import styles from './filters-sheet.module.scss';
@@ -27,7 +29,19 @@ export default function FiltersSheet({
 
   return (
     <animated.div style={{ overflow: 'hidden', ...props }}>
-      <div ref={ref} className={styles.wrapper} />
+      <div ref={ref} className={styles.wrapper}>
+        <form className={styles.form}>
+          <QueryInputs
+            value={query}
+            onChange={setQuery}
+            className={styles.field}
+            renderToPortal
+            availability
+            subjects
+            langs
+          />
+        </form>
+      </div>
     </animated.div>
   );
 }
