d37140bf4d61a98f5f82b25c80cf00502550a53b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-08-28 09:32:46 -0700

    fix(next.config.js): add 'INTERCOM_APP_ID' to env vars

--- next.config.js
+++ next.config.js
@@ -44,6 +44,7 @@ module.exports = withImages({
     return config;
   },
   env: {
+    INTERCOM_APP_ID: process.env.INTERCOM_APP_ID,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
