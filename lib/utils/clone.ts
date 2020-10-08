80ee987410320b95775b15af6c7ebda99263119d
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-10-07 22:04:19 -0700

    chore(deps): upgrade Typescript, use deep-cloning, and remove Jest

---
+++ lib/utils/clone.ts
@@ -0,0 +1,6 @@
+import rfdc from 'rfdc';
+
+// Temporary work-around to use the default options until they update the
+// Typescript definition file.
+// @see {@link https://github.com/davidmarkclements/rfdc/issues/14}
+export default rfdc();
