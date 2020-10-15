f0c261ce5d2371e4656857d7c664b99b9aac6261
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-10-14 17:45:45 -0700

    fix(lib/utils/clone): support nested 'Availability' objs

--- lib/utils/clone.ts
+++ lib/utils/clone.ts
@@ -1,6 +1,22 @@
 import rfdc from 'rfdc';
 
-// Temporary work-around to use the default options until they update the
-// Typescript definition file.
-// @see {@link https://github.com/davidmarkclements/rfdc/issues/14}
-export default rfdc();
+import { Availability, Timeslot } from 'lib/model';
+
+function cloneTime(time: Timeslot): Timeslot {
+  return new Timeslot(time.from, time.to, time.recur);
+}
+
+function cloneTimes(times: Availability): Availability {
+  return new Availability(...times.map(cloneTime));
+}
+
+type Temp = Record<'availability' | 'times', Availability>;
+
+// TODO: Perhaps try to get rid of all of these weird type workarounds.
+export default function clone<T>(obj: T): T {
+  const copy = (rfdc()(obj) as unknown) as Temp;
+  const orig = (obj as unknown) as Temp;
+  if (orig.availability) copy.availability = cloneTimes(orig.availability);
+  if (orig.times) copy.times = cloneTimes(orig.times);
+  return (copy as unknown) as T;
+}
