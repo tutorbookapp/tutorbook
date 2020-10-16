bd62658dfdef6aefa659f910bddd5cd221699ce2
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-10-16 14:59:08 -0700

    fix(lib/model): include falsy vals in database

--- lib/model/firestore-vals.ts
+++ lib/model/firestore-vals.ts
@@ -1,24 +1,13 @@
 /**
  * Converts a given data model object into a Firestore-valid datatype by
- * removing any empty values.
+ * removing any "undefined" values.
  * @param obj - The data model to clean.
- * @return The data model without any undefined or empty fields.
- * @todo When calling `DocumentReference#update`, this will prevent fields from
- * being properly cleared. We might want to change this default behavior and
- * include some empty fields.
+ * @return The data model without any "undefined" properties.
  */
 export default function firestoreVals<T>(obj: T): Partial<T> {
-  /* eslint-disable @typescript-eslint/no-unused-vars */
-  const allDefinedValues = Object.fromEntries(
+  return Object.fromEntries(
+    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+
     Object.entries(obj).filter(([_, val]) => val !== undefined)
-  );
-  const allFilledValues = Object.fromEntries(
-    Object.entries(allDefinedValues).filter(([_, val]) => {
-      if (!val) return false;
-      if (typeof val === 'object' && !Object.keys(val).length) return false;
-      return true;
-    })
-  );
-  return allFilledValues as Partial<T>;
-  /* eslint-enable @typescript-eslint/no-unused-vars */
+  ) as Partial<T>;
 }
