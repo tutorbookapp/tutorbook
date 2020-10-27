240c5faba537103600bdab21eb7b061af98d086f
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-10-27 10:31:44 -0700

    feat(api): add create org endpoint

--- pages/api/orgs/index.ts
+++ pages/api/orgs/index.ts
@@ -1,23 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { APIError } from 'lib/api/error';
+import createOrg, { CreateOrgRes } from 'lib/api/routes/orgs/create';
 import listOrgs, { ListOrgsRes } from 'lib/api/routes/orgs/list';
+import { APIError } from 'lib/api/error';
 
 /**
  * GET - Lists the orgs that the given user is a member of.
+ * POST - Creates a new org (the given user must be a member).
  *
  * Requires an authentication JWT.
  */
 export default async function orgs(
   req: NextApiRequest,
-  res: NextApiResponse<ListOrgsRes | APIError>
+  res: NextApiResponse<ListOrgsRes | CreateOrgRes | APIError>
 ): Promise<void> {
   switch (req.method) {
     case 'GET': // Fetch the org's profile document.
       await listOrgs(req, res);
       break;
+    case 'POST': // Create a new org.
+      await createOrg(req, res);
+      break;
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['GET', 'POST']);
       res.status(405).end(`Method ${req.method as string} Not Allowed`);
   }
 }
