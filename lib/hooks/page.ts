6c82aeb0095002e96afb6fb1a7a89369fc23fcd3
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-11-22 10:52:20 -0800

    0.19.21

--- lib/hooks/page.ts
+++ lib/hooks/page.ts
@@ -4,6 +4,6 @@ export default function usePage(name: string, orgId = 'default'): void {
   useEffect(() => {
     // The `orgId` prop is required to connect events with Mixpanel groups.
     // @see {@link https://bit.ly/36YrRsT}
-    window.analytics?.page(name, { orgId });
+    window.analytics?.page('', name, { orgId });
   }, [name, orgId]);
 }
