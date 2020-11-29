8d6323de13b0c5133aaa6abf23975dc73e0e4b76
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-11-28 21:34:40 -0800

    wip: add QuaranTunes email campaign tracking

--- lib/mail/emails/quarantunes.tsx
+++ lib/mail/emails/quarantunes.tsx
@@ -11,11 +11,15 @@ import {
 export interface CampaignEmailProps {
   name: string;
   email: string;
+  link: string;
+  pixel: string;
 }
 
 export default function CampaignEmail({
   name,
   email,
+  link,
+  pixel,
 }: CampaignEmailProps): JSX.Element {
   return (
     <Email>
@@ -23,26 +27,29 @@ export default function CampaignEmail({
       <Item>
         <P style={{ marginTop: '0px !important' }}>Hi {name},</P>
         <P>
-          QuaranTunes has just started using Tutorbook! You'll now be able to
-          set your own availability and manage your own profile page (without us
-          admins having to get involved).
+          QuaranTunes has just started using Tutorbook! You&apos;ll now be able
+          to set your own availability and manage your own profile page (without
+          us admins having to get involved).
         </P>
         <P>
-          This email (<b>{email}</b>) already has an account; we've imported
-          your Wix profile into Tutorbook.
+          You&apos;re also able to specify all the languages you speak! Please
+          add all your fluent languages to your profile; it&apos;ll come in
+          handy as we expand QuaranTunes&apos; reach.
         </P>
         <P>
-          To claim your account, login with this email address (<b>{email}</b>)
-          at the link below:
+          We&apos;ve already copied your existing QuaranTunes profile into
+          Tutorbook, but it&apos;s missing your availability and languages.
+        </P>
+        <P>
+          To claim and update your profile, login with this email address (
+          <b>{email}</b>) at the link below:
         </P>
         <br />
-        <Button href='https://tutorbook.app/profile'>CLAIM PROFILE</Button>
+        <Button href={link}>CLAIM PROFILE</Button>
         <br />
         <P>Or copy and paste this URL into a new tab of your browser:</P>
         <P style={{ marginBottom: '0px !important' }}>
-          <Link href='https://tutorbook.app/profile'>
-            {'https://tutorbook.app/profile'}
-          </Link>
+          <Link href={link}>{link}</Link>
         </P>
         <br />
         <P>
@@ -51,6 +58,7 @@ export default function CampaignEmail({
           Founder of{' '}
           <Link href='https://tutorbook.app/quarantunes'>QuaranTunes</Link>
         </P>
+        <img alt='' src={pixel} />
       </Item>
       <Footer>
         <P style={{ marginBottom: '0px !important' }}>
---
+++ pages/api/quarantunes.tsx
@@ -0,0 +1,52 @@
+import { NextApiRequest as Req, NextApiResponse as Res } from 'next';
+import Analytics from 'analytics-node';
+import { renderToStaticMarkup } from 'react-dom/server';
+
+import QuaranTunesEmail from 'lib/mail/emails/quarantunes';
+import { User } from 'lib/model';
+import { db } from 'lib/api/firebase';
+import send from 'lib/mail/send';
+
+export default async function quarantunes(req: Req, res: Res): Promise<void> {
+  const users = (
+    await db
+      .collection('users')
+      .where('orgs', 'array-contains', 'quarantunes')
+      .get()
+  ).docs.map((d) => User.fromFirestore(d));
+  const analytics = new Analytics(process.env.SEGMENT_KEY as string);
+  const baseURL = `http://${req.headers.host || 'tutorbook.app'}`;
+  return res.status(500).end('Endpoint Not Operational');
+  await Promise.all(
+    users.map(async (user: User) => {
+      analytics.identify({ userId: user.id, traits: user.toSegment() });
+      analytics.track({ userId: user.id, event: 'QuaranTunes Email Sent' });
+      const link =
+        `${baseURL}/profile?` +
+        `ajs_uid=${encodeURIComponent(user.id)}&` +
+        `ajs_event=${encodeURIComponent('QuaranTunes Email Link Clicked')}`;
+      const pixelJSON = JSON.stringify({
+        writeKey: process.env.SEGMENT_PIXEL_KEY as string,
+        event: 'QuaranTunes Email Opened',
+        userId: user.id,
+      });
+      const pixelData = Buffer.from(pixelJSON, 'utf-8').toString('base64');
+      const pixel = `https://api.segment.io/v1/pixel/track?data=${pixelData}`;
+      const firstName = user.name.split(' ')[0] || user.name;
+      return send({
+        replyTo: { name: 'Julia Segal', email: 'quarantunes.info@gmail.com' },
+        to: { name: user.name, email: user.email },
+        subject: `Hi ${firstName}! Could you update your QuaranTunes profile?`,
+        html: renderToStaticMarkup(
+          <QuaranTunesEmail
+            name={firstName}
+            email={user.email}
+            pixel={pixel}
+            link={link}
+          />
+        ),
+      });
+    })
+  );
+  res.status(201).end('QuaranTunes Emails Sent');
+}
