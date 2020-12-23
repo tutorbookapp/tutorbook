e915830c0e75b57245fee1cc172937cf4e59d481
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-12-23 10:54:54 -0800

    fix(lib/api/algolia): use new 'toSearchHit' methods

--- lib/api/algolia.ts
+++ lib/api/algolia.ts
@@ -5,46 +5,12 @@ import {
 } from '@algolia/client-search';
 import algoliasearch from 'algoliasearch';
 
-import {
-  Availability,
-  AvailabilitySearchHit,
-  Timeslot,
-  TimeslotSearchHit,
-} from 'lib/model';
 import clone from 'lib/utils/clone';
 
 const algoliaId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string;
 const algoliaKey = process.env.ALGOLIA_ADMIN_KEY as string;
 
 const client = algoliasearch(algoliaId, algoliaKey);
-
-function getIdxTime(time: Timeslot): TimeslotSearchHit {
-  return { ...time, from: time.from.valueOf(), to: time.to.valueOf() };
-}
-
-// TODO: Create `toSearchHit` methods for the `Availability` objects and use
-// those instead of creating these helper functions during indexing.
-function getIdxTimes(times: Availability): AvailabilitySearchHit {
-  return Array.from(times.map(getIdxTime));
-}
-
-interface Temp {
-  availability: Availability;
-  time: Timeslot;
-}
-
-interface TempSearchHit {
-  availability: AvailabilitySearchHit;
-  time: TimeslotSearchHit;
-}
-
-// TODO: Remove all of these ugly Typescript workarounds.
-function updateTimes(idxObj: unknown): void {
-  const obj = idxObj as Temp;
-  const hit = idxObj as TempSearchHit;
-  if (obj.availability) hit.availability = getIdxTimes(obj.availability);
-  if (obj.time) hit.time = getIdxTime(obj.time);
-}
 
 export function updateFilterableAttrs(
   indexId: string,
@@ -65,18 +31,10 @@ export function deleteObj(
   return (idx.deleteObject(objId) as unknown) as Promise<DeleteResponse>;
 }
 
-export default function index<T extends { id: string }>(
-  indexId: string,
-  obj: T,
-  tags?: string[]
-): Promise<SaveObjectResponse> {
+export default function index<
+  T extends { toSearchHit: () => Record<string, unknown> }
+>(indexId: string, obj: T, tags?: string[]): Promise<SaveObjectResponse> {
   const idx = client.initIndex(`${process.env.APP_ENV as string}-${indexId}`);
-  const idxObj = clone<Omit<T, 'id'> & { id?: string }>({
-    ...obj,
-    _tags: tags,
-    objectID: obj.id,
-  });
-  delete idxObj.id;
-  updateTimes(idxObj);
+  const idxObj = clone({ ...obj.toSearchHit(), _tags: tags });
   return (idx.saveObject(idxObj) as unknown) as Promise<SaveObjectResponse>;
 }
