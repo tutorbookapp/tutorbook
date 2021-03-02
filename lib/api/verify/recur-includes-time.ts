7d9f3dba04dca0f4f40c8b24335ba4694e7ac17c
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-01 21:26:56 -0800

    fix(api/verify/recur-includes-time): recur is optional

--- lib/api/verify/recur-includes-time.ts
+++ lib/api/verify/recur-includes-time.ts
@@ -4,7 +4,10 @@ import { Timeslot } from 'lib/model';
 
 // Ensure that the `until` value of an RRule isn't before the `time.from` value
 // (which would prevent *any* meeting instances from being calculated).
-export default function verifyRecurIncludesTime(time: Timeslot): string {
+export default function verifyRecurIncludesTime(
+  time: Timeslot
+): string | undefined {
+  if (!time.recur) return time.recur;
   const { until, ...options } = RRule.parseString(time.recur);
   if (!until || until >= time.to) return time.recur;
   // If the `until` value is before the timeslot end, we set it to the timeslot
