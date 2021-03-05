a97f85e4ff819b54422a4f556bbdb8989ace07d3
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-04 20:41:40 -0800

    wip(calendar): disable delete functionality

--- components/calendar/dialogs/edit/display-page.tsx
+++ components/calendar/dialogs/edit/display-page.tsx
@@ -1,19 +1,11 @@
 import { Chip, ChipSet } from '@rmwc/chip';
-import axios, { AxiosError } from 'axios';
-import { useCallback, useEffect, useState } from 'react';
 import Link from 'next/link';
-import to from 'await-to-js';
-import useTranslation from 'next-translate/useTranslation';
 
 import Avatar from 'components/avatar';
 
-import { APIErrorJSON } from 'lib/api/error';
 import { Meeting } from 'lib/model/meeting';
 import { User } from 'lib/model/user';
 import { join } from 'lib/utils';
-import snackbar from 'lib/snackbar';
-
-import { useCalendar } from '../../context';
 
 import styles from './display-page.module.scss';
 
@@ -28,46 +20,6 @@ export default function DisplayPage({
   meeting,
   openEdit,
 }: DisplayPageProps): JSX.Element {
-  const { t } = useTranslation();
-  const { removeMeeting } = useCalendar();
-
-  const [error, setError] = useState<string>('');
-  const deleteMeeting = useCallback(async () => {
-    setError('');
-    console.log('Delete checked:', false);
-    console.log('Delete loading:', true);
-    const endpoint = `/api/meetings/${meeting.parentId || meeting.id}`;
-    const [err] = await to(axios.delete(endpoint));
-    if (err) {
-      const e = (err as AxiosError<APIErrorJSON>).response?.data || err;
-      console.log('Delete loading:', false);
-      setError(e.message);
-    } else {
-      console.log('Delete checked:', true);
-      setTimeout(() => {
-        // TODO: Locally mutate and remove all meetings with `parentId`.
-        void removeMeeting(meeting.parentId || meeting.id, true);
-      }, 1000);
-    }
-  }, [removeMeeting, meeting.id, meeting.parentId]);
-
-  useEffect(() => {
-    // TODO: Close snackbar when delete button is clicked repeatedly (i.e. when
-    // error is reset; similar to how it works when using the component).
-    if (error)
-      snackbar.notify({
-        body: t('meeting:delete-error'),
-        actions: [
-          {
-            label: t('common:retry'),
-            onClick: deleteMeeting,
-          },
-        ],
-        onClose: () => setError(''),
-        dismissesOnAction: true,
-      });
-  }, [error, deleteMeeting, meeting.id, t]);
-
   return (
     <>
       <div className={styles.content}>
@@ -103,7 +55,7 @@ export default function DisplayPage({
       <div className={styles.actions}>
         <ChipSet className={styles.chips}>
           <Chip icon='edit' label='Edit meeting' onClick={openEdit} />
-          <Chip icon='delete' label='Delete meeting' onClick={deleteMeeting} />
+          <Chip icon='delete' label='Delete meeting' />
         </ChipSet>
       </div>
     </>
