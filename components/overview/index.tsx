3879b3e534fb33306f0690b8eae34871587a4e3e
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-11 23:32:11 -0800

    wip(overview): add placeholder numbers

--- components/overview/index.tsx
+++ components/overview/index.tsx
@@ -1,11 +1,32 @@
+import cn from 'classnames';
 import useTranslation from 'next-translate/useTranslation';
 
 import Header from 'components/header';
-import Placeholder from 'components/placeholder';
 
 import { useOrg } from 'lib/context/org';
 
 import styles from './overview.module.scss';
+
+interface LabelProps {
+  percent: number;
+  positive?: boolean;
+  negative?: boolean;
+}
+
+function Label({ percent, positive, negative }: LabelProps): JSX.Element {
+  return (
+    <span
+      className={cn(styles.label, {
+        [styles.positive]: positive,
+        [styles.negative]: negative,
+      })}
+    >
+      {positive && '+'}
+      {negative && '-'}
+      {`${percent}%`}
+    </span>
+  );
+}
 
 export default function Overview(): JSX.Element {
   const { t } = useTranslation();
@@ -18,7 +39,40 @@ export default function Overview(): JSX.Element {
         body={t('overview:subtitle', { name: org ? `${org.name}'s` : 'your' })}
       />
       <div className={styles.wrapper}>
-        <Placeholder>{t('overview:placeholder')}</Placeholder>
+        <dl className={styles.numbers}>
+          <div className={styles.number}>
+            <dt>
+              Volunteers
+              <Label percent={12.5} positive />
+            </dt>
+            <dd>258</dd>
+            <div>189 Matched</div>
+          </div>
+          <div className={styles.number}>
+            <dt>
+              Students
+              <Label percent={12.5} positive />
+            </dt>
+            <dd>218</dd>
+            <div>218 Matched</div>
+          </div>
+          <div className={styles.number}>
+            <dt>
+              Matches
+              <Label percent={2.3} negative />
+            </dt>
+            <dd>443</dd>
+            <div>85% Meeting</div>
+          </div>
+          <div className={styles.number}>
+            <dt>
+              Meetings
+              <Label percent={32.5} positive />
+            </dt>
+            <dd>5,425</dd>
+            <div>546 Recurring</div>
+          </div>
+        </dl>
       </div>
     </>
   );
