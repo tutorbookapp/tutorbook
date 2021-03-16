78fd420baf9e625b7193cf7c946cd9bdd299b5f0
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-15 22:37:02 -0700

    fix(pages/500): add custom static 500 error page
    Addresses the changes made by vercel/next.js#22139 by adding another
    custom static 500 page (exactly the same as the custom 404 page but for
    server-side errors).

---
+++ pages/500.tsx
@@ -0,0 +1,31 @@
+import useTranslation from 'next-translate/useTranslation';
+
+import { EmptyHeader } from 'components/navigation';
+import Notification from 'components/notification';
+import Page from 'components/page';
+
+import { PageProps, getPageProps } from 'lib/page';
+import usePage from 'lib/hooks/page';
+import { withI18n } from 'lib/intl';
+
+import common from 'locales/en/common.json';
+import error from 'locales/en/error.json';
+
+function Custom500Page(props: PageProps): JSX.Element {
+  const { t } = useTranslation();
+
+  usePage({ name: '500' });
+
+  return (
+    <Page title={`${t('error:500-header')} - Tutorbook`} intercom {...props}>
+      <EmptyHeader />
+      <Notification header={t('error:500-header')}>
+        <p>{t('error:500-body')}</p>
+      </Notification>
+    </Page>
+  );
+}
+
+export const getStaticProps = getPageProps;
+
+export default withI18n(Custom500Page, { common, error });
