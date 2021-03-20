6461733eeef5d7db2e6102a70f7915a3ac561031
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-20 14:18:04 -0700

    fix(lib/api/global.d.ts): update 'await-to-js' types
    Adds the changes in scopsy/await-to-js#46 to our local type definitions
    (which exist because I'm waiting on scopsy/await-to-js#47).

--- lib/api/global.d.ts
+++ lib/api/global.d.ts
@@ -2,5 +2,5 @@ declare module 'await-to-js' {
   export default function to<T, U = Error>(
     promise: Readonly<Promise<T>>,
     errorExt?: object
-  ): Promise<[U | null, T | undefined]>;
+  ): Promise<[U, undefined] | [null, T]>;
 }
