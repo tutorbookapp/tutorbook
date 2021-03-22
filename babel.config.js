d3c29025419984cc66fb48ad380e0f77a5193a0d
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-21 20:36:17 -0700

    perf: only import Recharts modules we actually use
    Because recharts/babel-plugin-recharts#22 is merged, we can now only
    include the Recharts modules that we actually use in production.

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,12 @@
  * @see {@link https://babeljs.io/docs/en/configuration#babelconfigjson}
  * @see {@link https://docs.cypress.io/guides/tooling/code-coverage.html#Instrumenting-code}
  *
- * Only include the recharts modules we actually need.
- * @todo Once TypeScript support has been merged, add the `recharts` plugin.
+ * Only includes the recharts modules we actually need.
  * @see {@link https://github.com/recharts/babel-plugin-recharts/pull/22}
  * @see {@link https://github.com/recharts/babel-plugin-recharts}
  */
 module.exports = {
   presets: ['next/babel'],
-  plugins: process.env.NODE_ENV === 'test' ? ['istanbul'] : [],
+  plugins:
+    process.env.NODE_ENV === 'test' ? ['recharts', 'istanbul'] : ['recharts'],
 };
