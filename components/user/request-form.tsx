5c7cb6f652f0da06bcc70d5ec027fba57abd2ac1
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-22 23:04:35 -0700

    fix(user/request-form): remove unused import

--- components/user/request-form.tsx
+++ components/user/request-form.tsx
@@ -1,11 +1,4 @@
-import {
-  FormEvent,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import { FormEvent, useCallback, useEffect, useMemo, useState } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Select } from '@rmwc/select';
 import { TextField } from '@rmwc/textfield';
