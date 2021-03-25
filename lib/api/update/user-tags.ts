985928bde86c941b70bc3a131686c28c24e06610
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-25 12:18:38 -0700

    fix: add parent roles to people tags
    Closes: https://app.clickup.com/t/qk0z0d

--- lib/api/update/user-tags.ts
+++ lib/api/update/user-tags.ts
@@ -5,16 +5,12 @@ export default function updateUserTags(
   user: User,
   actions?: { add?: UserTag[]; remove?: UserTag[] }
 ): User {
-  const tags = new Set<UserTag>(user.tags);
+  const tags = new Set<UserTag>([...user.tags, ...user.roles]);
 
-  if (user.mentoring.subjects.length || user.roles.includes('mentor'))
-    tags.add('mentor');
-  if (user.mentoring.searches.length || user.roles.includes('mentee'))
-    tags.add('mentee');
-  if (user.tutoring.subjects.length || user.roles.includes('tutor'))
-    tags.add('tutor');
-  if (user.tutoring.searches.length || user.roles.includes('tutee'))
-    tags.add('tutee');
+  if (user.mentoring.subjects.length) tags.add('mentor');
+  if (user.mentoring.searches.length) tags.add('mentee');
+  if (user.tutoring.subjects.length) tags.add('tutor');
+  if (user.tutoring.searches.length) tags.add('tutee');
 
   if (user.verifications.length) tags.add('vetted');
   if (!user.verifications.length) tags.delete('vetted');
