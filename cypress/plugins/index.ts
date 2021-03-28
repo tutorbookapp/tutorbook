8b94edb958318d5988302b8dc53274cf9ef85ed9
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-27 17:09:17 -0700

    test: remove log debugging commands
    This reverts commit db9f7693fa3295cebafb78070684f7e7e7fc183c.

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -7,7 +7,6 @@ import client from 'firebase/app';
 import codecov from '@cypress/code-coverage/task';
 import dotenv from 'dotenv';
 import firebase from 'firebase-admin';
-import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter';
 import { percyHealthCheck } from '@percy/cypress/task';
 
 import { MatchJSON } from 'lib/model/match';
@@ -108,7 +107,6 @@ export default function plugins(
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
 ): Cypress.ConfigOptions {
-  installLogsPrinter(on);
   codecov(on, config);
   on('task', {
     percyHealthCheck,
@@ -216,8 +214,6 @@ export default function plugins(
       }
 
       await create('orgs', orgs);
-
-      console.log('Creating users:', users);
 
       // We have to create the admin first because TB's back-end will try to
       // fetch his data when sending user creation notification emails.
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,13 +1,6 @@
-import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector';
-
 import '@cypress/code-coverage/support';
 
 import './commands';
-
-installLogsCollector({
-  enableExtendedCollector: false,
-  xhr: { printHeaderData: true, printRequestData: true },
-});
 
 before(() => {
   cy.task('createIndices');
