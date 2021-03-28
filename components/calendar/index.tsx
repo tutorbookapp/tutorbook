tutorbookapp/tutorbook

commit 74265904c92f78d8f1a79cbb7c4b43d7e8f905b1

feat: manage MeetingsQuery with URL params

components/calendar/index.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react';
 import axios, { AxiosError } from 'axios';
 import useSWR, { mutate } from 'swr';
+import Router from 'next/router';
 import { Snackbar } from '@rmwc/snackbar';
 import { dequal } from 'dequal/lite';
 import to from 'await-to-js';
@@ -274,6 +275,25 @@ export default function Calendar({
     },
     [editing.parentId, onDeleteSubmit]
   );
+
+  useEffect(() => {
+    // TODO: Ideally, we'd be able to use Next.js's `useRouter` hook to get the
+    // URL query parameters, but right now, it doesn't seem to be working. Once
+    // we do replace this with the `useRouter` hook, we'll be able to replace
+    // state management with just shallowly updating the URL.
+    // @see {@link https://github.com/vercel/next.js/issues/17112}
+    if (typeof window === 'undefined') return;
+    const params = new URLSearchParams(window.location.search);
+    setQuery(MeetingsQuery.fromURLParams(Object.fromEntries(params.entries())));
+  }, []);
+
+  // TODO: Use the built-in Next.js router hook to manage this query state and
+  // show the `NProgress` loader when the results are coming in.
+  useEffect(() => {
+    if (!org || !org.id) return;
+    const url = query.getURL(`/${org.id}/calendar`);
+    void Router.replace(url, undefined, { shallow: true });
+  }, [org, query]);
 
   return (
     <CalendarStateContext.Provider value={calendarState}>

commit cfd822a76c229c08213479a2845d6b35f4f6bbb5

fix: don't override default page query state
Fixes an issue introduced in #151 where the custom page query state
would be overridden by default query properties (that aren't specified
in URL query parameters).

Instead, the default (first-render) page query state is used to fill
fields that are not specified in the URL query parameters.

components/calendar/index.tsx
@@ -8,7 +8,6 @@ import {
 } from 'react';
 import axios, { AxiosError } from 'axios';
 import useSWR, { mutate } from 'swr';
-import Router from 'next/router';
 import { Snackbar } from '@rmwc/snackbar';
 import { dequal } from 'dequal/lite';
 import to from 'await-to-js';
@@ -24,6 +23,7 @@ import { Position } from 'lib/model/position';
 import { useOrg } from 'lib/context/org';
 import usePeople from 'lib/hooks/people';
 import useSingle from 'lib/hooks/single';
+import useURLParamSync from 'lib/hooks/url-param-sync';
 import { useUser } from 'lib/context/user';
 
 import { CalendarStateContext, DialogPage } from './state';
@@ -51,9 +51,9 @@ export default function Calendar({
 }: CalendarProps): JSX.Element {
   const [filtersOpen, setFiltersOpen] = useState<boolean>(false);
   const [mutatedIds, setMutatedIds] = useState<Set<string>>(new Set());
-  const [query, setQuery] = useState<MeetingsQuery>(
-    new MeetingsQuery({ hitsPerPage: 1000 })
-  );
+  const [query, setQuery] = useState<MeetingsQuery>(new MeetingsQuery());
+
+  useURLParamSync(query, setQuery, MeetingsQuery);
 
   const { org } = useOrg();
   const { user } = useUser();
@@ -275,25 +275,6 @@ export default function Calendar({
     },
     [editing.parentId, onDeleteSubmit]
   );
-
-  useEffect(() => {
-    // TODO: Ideally, we'd be able to use Next.js's `useRouter` hook to get the
-    // URL query parameters, but right now, it doesn't seem to be working. Once
-    // we do replace this with the `useRouter` hook, we'll be able to replace
-    // state management with just shallowly updating the URL.
-    // @see {@link https://github.com/vercel/next.js/issues/17112}
-    if (typeof window === 'undefined') return;
-    const params = new URLSearchParams(window.location.search);
-    setQuery(MeetingsQuery.fromURLParams(Object.fromEntries(params.entries())));
-  }, []);
-
-  // TODO: Use the built-in Next.js router hook to manage this query state and
-  // show the `NProgress` loader when the results are coming in.
-  useEffect(() => {
-    if (!org || !org.id) return;
-    const url = query.getURL(`/${org.id}/calendar`);
-    void Router.replace(url, undefined, { shallow: true });
-  }, [org, query]);
 
   return (
     <CalendarStateContext.Provider value={calendarState}>

components/users/index.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useEffect, useState } from 'react';
-import Router from 'next/router';
 import { dequal } from 'dequal/lite';
 
 import Pagination from 'components/pagination';
@@ -7,6 +6,7 @@ import Pagination from 'components/pagination';
 import { CallbackParam } from 'lib/model/callback';
 import { UsersQuery } from 'lib/model/query/users';
 import { useOrg } from 'lib/context/org';
+import useURLParamSync from 'lib/hooks/url-param-sync';
 
 import FiltersSheet from './filters-sheet';
 import Header from './header';
@@ -40,6 +40,8 @@ export default function Users(): JSX.Element {
   );
   const [hits, setHits] = useState<number>(query.hitsPerPage);
 
+  useURLParamSync(query, setQuery, UsersQuery);
+
   const onQueryChange = useCallback((param: CallbackParam<UsersQuery>) => {
     setQuery((prev) => {
       let updated = prev;
@@ -50,25 +52,6 @@ export default function Users(): JSX.Element {
       return updated;
     });
   }, []);
-
-  useEffect(() => {
-    // TODO: Ideally, we'd be able to use Next.js's `useRouter` hook to get the
-    // URL query parameters, but right now, it doesn't seem to be working. Once
-    // we do replace this with the `useRouter` hook, we'll be able to replace
-    // state management with just shallowly updating the URL.
-    // @see {@link https://github.com/vercel/next.js/issues/17112}
-    if (typeof window === 'undefined') return;
-    const params = new URLSearchParams(window.location.search);
-    setQuery(UsersQuery.fromURLParams(Object.fromEntries(params.entries())));
-  }, []);
-
-  // TODO: Use the built-in Next.js router hook to manage this query state and
-  // show the `NProgress` loader when the results are coming in.
-  useEffect(() => {
-    if (!org || !org.id) return;
-    const url = query.getURL(`/${org.id}/users`);
-    void Router.replace(url, undefined, { shallow: true });
-  }, [org, query]);
 
   useEffect(() => {
     onQueryChange((prev) => {

lib/hooks/url-param-sync.ts
@@ -1,0 +1,41 @@
+import Router from 'next/router';
+import { dequal } from 'dequal';
+import { useEffect } from 'react';
+
+import { Callback } from 'lib/model/callback';
+import { Query } from 'lib/model/query/base';
+
+interface Constructor<T extends Query> {
+  fromURLParams: (params: any) => T;
+}
+
+// Syncs the initial query state in URL parameters. This gives users a sharable
+// link that contains their query state (e.g. I can share a link showing all the
+// Songwriting teachers with another org admin).
+// TODO: Perhaps look into directly managing state using the Next.js router.
+export default function useURLParamSync<T extends Query>(
+  query: T,
+  setQuery: Callback<T>,
+  Model: Constructor<T>
+): void {
+  useEffect(() => {
+    setQuery((prev) => {
+      if (typeof window === 'undefined') return prev;
+      const params = new URLSearchParams(window.location.search);
+      const updated = Model.fromURLParams({
+        ...prev.getURLParams(),
+        ...Object.fromEntries(params.entries()),
+      });
+      if (dequal(prev, updated)) return prev;
+      return updated;
+    });
+  }, [setQuery, Model]);
+
+  // TODO: Don't include query params that are specified in other ways (e.g. the
+  // users dashboard specifies org in the `[org]` dynamic page param).
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+    const url = query.getURL(window.location.pathname);
+    void Router.replace(url, undefined, { shallow: true });
+  }, [query]);
+}

lib/model/query/base.ts
@@ -45,7 +45,7 @@ export abstract class Query implements QueryInterface {
 
   public abstract get endpoint(): string;
 
-  protected getURLQuery(): Record<string, string | number | boolean> {
+  public getURLParams(): Record<string, string | number | boolean> {
     const query: Record<string, string | number | boolean> = {};
     if (this.search) query.search = encodeURIComponent(this.search);
     if (this.hitsPerPage !== 20) query.hitsPerPage = this.hitsPerPage;
@@ -54,7 +54,7 @@ export abstract class Query implements QueryInterface {
   }
 
   public getURL(pathname: string): string {
-    return url.format({ pathname, query: this.getURLQuery() });
+    return url.format({ pathname, query: this.getURLParams() });
   }
 
   public static fromURLParams(params: QueryURL): QueryInterface {

lib/model/query/matches.ts
@@ -29,12 +29,12 @@ export class MatchesQuery extends Query implements MatchesQueryInterface {
     construct<MatchesQueryInterface>(this, query);
   }
 
-  protected getURLQuery(): Record<string, string | number | boolean> {
+  public getURLParams(): Record<string, string | number | boolean> {
     function encode(p?: Option<any>[]): string {
       return encodeURIComponent(JSON.stringify(p));
     }
 
-    const query = super.getURLQuery();
+    const query = super.getURLParams();
     if (this.org) query.org = encodeURIComponent(this.org);
     if (this.people.length) query.people = encode(this.people);
     if (this.subjects.length) query.subjects = encode(this.subjects);

lib/model/query/meetings.ts
@@ -58,12 +58,17 @@ export class MeetingsQuery extends MatchesQuery
     );
   }
 
-  protected getURLQuery(): Record<string, string | number | boolean> {
+  public getURLParams(): Record<string, string | number | boolean> {
     function encode(p?: any[]): string {
       return encodeURIComponent(JSON.stringify(p));
     }
 
-    const query = super.getURLQuery();
+    const query = super.getURLParams();
+    if (this.hitsPerPage !== 1000) {
+      query.hitsPerPage = this.hitsPerPage;
+    } else {
+      delete query.hitsPerPage;
+    }
     if (this.tags.length) query.tags = encode(this.tags);
     query.from = this.from.toJSON();
     query.to = this.to.toJSON();

lib/model/query/users.ts
@@ -71,12 +71,12 @@ export class UsersQuery extends Query implements UsersQueryInterface {
   // and deserializing the complex `Option` JSON objects.
   // @see {@link https://www.npmjs.com/package/serialize-query-params}
   // @see {@link https://github.com/pbeshai/use-query-params}
-  protected getURLQuery(): Record<string, string | number | boolean> {
+  public getURLParams(): Record<string, string | number | boolean> {
     function encode(p?: any[]): string {
       return encodeURIComponent(JSON.stringify(p));
     }
 
-    const query = super.getURLQuery();
+    const query = super.getURLParams();
     if (this.parents.length) query.parents = encode(this.parents);
     if (this.orgs.length) query.orgs = encode(this.orgs);
     if (this.tags.length) query.tags = encode(this.tags);

pages/[org]/search.tsx
@@ -4,7 +4,6 @@ import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import useSWR, { mutate } from 'swr';
 import NProgress from 'nprogress';
-import Router from 'next/router';
 import { dequal } from 'dequal/lite';
 import useTranslation from 'next-translate/useTranslation';
 
@@ -26,6 +25,7 @@ import { prefetch } from 'lib/fetch';
 import useAnalytics from 'lib/hooks/analytics';
 import usePage from 'lib/hooks/page';
 import useTrack from 'lib/hooks/track';
+import useURLParamSync from 'lib/hooks/url-param-sync';
 import { useUser } from 'lib/context/user';
 import { withI18n } from 'lib/intl';
 
@@ -49,6 +49,8 @@ function SearchPage({ org, ...props }: SearchPageProps): JSX.Element {
   const [auth, setAuth] = useState<boolean>(false);
   const [canSearch, setCanSearch] = useState<boolean>(false);
   const [searching, setSearching] = useState<boolean>(true);
+
+  useURLParamSync(query, setQuery, UsersQuery);
 
   const { data, isValidating } = useSWR<ListUsersRes>(
     canSearch ? query.endpoint : null
@@ -90,25 +92,6 @@ function SearchPage({ org, ...props }: SearchPageProps): JSX.Element {
     );
     void prefetch(nextPageQuery.endpoint);
   }, [query]);
-
-  useEffect(() => {
-    // TODO: Ideally, we'd be able to use Next.js's `useRouter` hook to get the
-    // URL query parameters, but right now, it doesn't seem to be working. Once
-    // we do replace this with the `useRouter` hook, we'll be able to replace
-    // state management with just shallowly updating the URL.
-    // @see {@link https://github.com/vercel/next.js/issues/17112}
-    if (typeof window === 'undefined') return;
-    const params = new URLSearchParams(window.location.search);
-    setQuery(UsersQuery.fromURLParams(Object.fromEntries(params.entries())));
-  }, []);
-
-  // TODO: Use the built-in Next.js router hook to manage this query state and
-  // show the `NProgress` loader when the results are coming in.
-  useEffect(() => {
-    if (!org || !org.id) return;
-    const url = query.getURL(`/${org.id}/search`);
-    void Router.replace(url, undefined, { shallow: true });
-  }, [org, query]);
 
   // TODO: Perhaps we should only allow filtering by a single org, as we don't
   // ever filter by more than one at once.