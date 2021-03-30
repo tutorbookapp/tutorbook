32a91daf77538990412325d5109a6b62333e3260
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-30 15:23:18 -0700

    test: update paragraph selectors for Next.js 10.1
    The new minor version of Next.js (10.1) uses a `p` tag for an internal
    build watcher component or something similar.
    
    This updates our `p` selectors to take that into account and only select
    paragraphs that occur within the `page`.

--- cypress/tests/authentication-failed.spec.ts
+++ cypress/tests/authentication-failed.spec.ts
@@ -6,20 +6,23 @@ describe('Authentication failed page', () => {
   });
 
   it('shows authentication failed message', () => {
-    cy.getBySel('page').find('h3').should('have.text', 'Authentication Failed');
-    cy.get('p')
-      .should('have.length', 2)
-      .first()
-      .should(
-        'have.text',
-        'It looks like you may have clicked on an invalid email verification link.'
-      );
-    cy.get('p')
-      .last()
-      .should(
-        'have.text',
-        'Please close this window and try authenticating again.'
-      );
+    cy.getBySel('page').within(() => {
+      cy.get('h3').should('have.text', 'Authentication Failed');
+      cy.get('p')
+        .should('have.length', 2)
+        .first()
+        .should(
+          'have.text',
+          'It looks like you may have clicked on an invalid email verification link.'
+        );
+      cy.get('p')
+        .last()
+        .should(
+          'have.text',
+          'Please close this window and try authenticating again.'
+        );
+    });
+
     cy.wait('@get-account');
     cy.percySnapshot('Authentication Failed Page');
   });
