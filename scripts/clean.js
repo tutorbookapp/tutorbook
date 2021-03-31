11d982904a72791ef7c9569e57d6df1679853975
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-31 14:36:43 -0700

    chore: add Algolia index cleanup script

---
+++ scripts/clean.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const algoliasearch = require('algoliasearch');
+const dotenv = require('dotenv');
+
+// Follow the Next.js convention for loading `.env` files.
+// @see {@link https://nextjs.org/docs/basic-features/environment-variables}
+const env = process.env.NODE_ENV || 'development';
+[
+  path.resolve(__dirname, `../.env.${env}.local`),
+  path.resolve(__dirname, '../.env.local'),
+  path.resolve(__dirname, `../.env.${env}`),
+  path.resolve(__dirname, '../.env'),
+].forEach((dotfile) => {
+  console.log(`Loaded env from ${dotfile}`);
+  dotenv.config({ path: dotfile });
+});
+
+const algoliaId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
+const algoliaKey = process.env.ALGOLIA_ADMIN_KEY;
+const search = algoliasearch(algoliaId, algoliaKey);
+
+const prefix = process.env.ALGOLIA_PREFIX || env;
+const usersIdx = search.initIndex(`${prefix}-users`);
+const matchesIdx = search.initIndex(`${prefix}-matches`);
+const meetingsIdx = search.initIndex(`${prefix}-meetings`);
+
+function deleteIndices() {
+  console.log('Deleting indices...');
+  return Promise.all([
+    usersIdx.delete(),
+    matchesIdx.delete(),
+    meetingsIdx.delete(),
+  ]);
+}
+
+async function main() {
+  await deleteIndices();
+}
+
+if (require.main === module) main();
