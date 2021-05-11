5c14492c9c5283b7b2c024bf5bb05e15a32f3ad3
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-10 21:48:42 -0700

    fix(pages/document): workaround for Firefox FOUC
    See: vercel/next.js#22465

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+// Prevent FOUC on Firefox due to an age-old script processing bug.
+// @see {@link https://nextjs.org/docs/advanced-features/custom-document}
+// @see {@link https://github.com/vercel/next.js/issues/22465}
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <script>0</script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
