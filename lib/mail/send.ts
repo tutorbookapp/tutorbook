14ef87a863c860889709ee4c84e7f1bd1673cc79
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-11 20:42:48 -0700

    chore(mail/send): disable emails during import

--- lib/mail/send.ts
+++ lib/mail/send.ts
@@ -1,24 +1,5 @@
-import mail from '@sendgrid/mail';
-import to from 'await-to-js';
-
-import { APIError } from 'lib/api/error';
 import { Email } from 'lib/mail/types';
 
 export default async function send(email: Email): Promise<void> {
-  if (['development', 'test'].includes(process.env.APP_ENV as string)) return;
-  if (typeof process.env.SENDGRID_API_KEY !== 'string') {
-    throw new APIError('Cannot send emails without SendGrid API key.');
-  } else {
-    mail.setApiKey(process.env.SENDGRID_API_KEY);
-    const [e] = await to(
-      mail.send({
-        ...email,
-        from: { name: 'Tutorbook', email: 'team@tutorbook.org' },
-        bcc: { name: 'Tutorbook', email: 'team@tutorbook.org' },
-        replyTo: email.replyTo?.email ? email.replyTo : undefined,
-        to: email.to.filter((p) => p.email),
-      })
-    );
-    if (e) throw new APIError(`${e.name} sending email: ${e.message}`, 500);
-  }
+  return;
 }
