f5104e47702dafc8b6536584ea388489465e2603
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-15 15:36:55 -0700

    feat(firebase/functions): rename to just 'remind'

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 // @see {@link https://bit.ly/3w66RLK}
 // We use the event timestamp to correctly pick our time windows.
 // @see {@link https://bit.ly/3fp8mOk}
-export const sendReminders = functions.pubsub
+export const remind = functions.pubsub
   .schedule('every 1 hours synchronized')
   .onRun(({ timestamp }) => {
     const { token } = functions.config().remind as { token: string };
