e8ad66da4a4973d84c8a5eee7b9e4002b92e4d9b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-06-15 03:27:12 +0000

    feat(scripts): add Supabase playground script

---
+++ scripts/migrate/supabase/playground.js
@@ -0,0 +1,18 @@
+// Playground script to test the Supabase Node.js client.
+
+const supabase = require('../../lib/supabase');
+const firebase = require('../../lib/firebase');
+const logger = require('../../lib/logger');
+
+async function playground() {
+  const { data, error } = await supabase
+    .from('relation_parents')
+    .select(`
+      user (*),
+      parent (*)
+    `);
+  logger.info(JSON.stringify(data[0], null, 2));
+  debugger;
+}
+
+if (require.main === module) playground();
