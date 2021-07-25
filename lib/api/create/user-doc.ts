5463f5c4542667a2ea1cb52d255ff8e4d8e63a4e
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-07-25 13:18:33 -0700

    fix(api/user-doc): save relations to separate tables

--- lib/api/create/user-doc.ts
+++ lib/api/create/user-doc.ts
@@ -1,9 +1,15 @@
 import { APIError } from 'lib/api/error';
 import { User } from 'lib/model/user';
+import clone from 'lib/utils/clone';
 import supabase from 'lib/api/supabase';
 
 export default async function createUserDoc(user: User): Promise<User> {
-  const { data, error } = await supabase.from('users').insert(user);
+  // TODO: Insert these relations into the proper relation table.
+  const copy: Partial<User> = clone(user);
+  delete copy.orgs;
+  delete copy.roles;
+  delete copy.parents;
+  const { data, error } = await supabase.from('users').insert(copy);
   if (error) {
     const msg = `Error saving user (${user.toString()}) in database`;
     throw new APIError(`${msg}: ${error.message}`, 500);
