e9505c05468d8ddfc09d90eb9b9c7090ecf6aa53
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-07-25 15:47:50 -0700

    fix(api/error): remove `debugger` statement

--- lib/api/error.ts
+++ lib/api/error.ts
@@ -49,10 +49,8 @@ export function handle(e: unknown, res: ServerResponse): void {
   if (!(e instanceof APIError) || e.code !== 401)
     console.error('API endpoint encountered error:', e);
   if (e instanceof APIError) return send(e, res);
-  if (e instanceof ZodError) {
-    debugger;
+  if (e instanceof ZodError)
     return send(new APIError(e.message, 400, 'Type Error'), res);
-  }
   if (e instanceof Error) return send(new APIError(e.message, 500), res);
   if (typeof e === 'string') return send(new APIError(e, 500), res);
   return send(new APIError('Unknown error', 500), res);
