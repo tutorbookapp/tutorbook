7f05eae76201ec7b7c00b48de3507f024d36b6cb
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-07-31 20:16:19 -0700

    refactor(lib/api): move `create/` files to `db/`

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,3 +1,27 @@
+import to from 'await-to-js';
+
+import { FirebaseError, auth } from 'lib/api/firebase';
+import { APIError } from 'lib/api/error';
+
+/**
+ * Creates a custom login token that can be used by the client when they signup
+ * with the deprecated org signup page.
+ * @param uid - The user ID of the user to create the login token for.
+ * @return Promise that resolves with the token string; throws an `APIError` if
+ * we were unable to create a custom token.
+ * @todo Remove this once we fix #116 and get rid of that signup page.
+ */
+export default async function createCustomToken(uid: string): Promise<string> {
+  const [err, token] = await to<string, FirebaseError>(
+    auth.createCustomToken(uid)
+  );
+  if (err) {
+    const msg = `${err.name} (${err.code}) creating custom auth token`;
+    throw new APIError(`${msg} for user (${uid}): ${err.message}`, 500);
+  }
+  return token as string;
+}
+
 import phone from 'phone';
 import to from 'await-to-js';
 import { v4 as uuid } from 'uuid';
