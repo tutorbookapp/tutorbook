c5d03fefe9ffc84a58f9a478bfab4eebd13ac2d9
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-07 09:09:00 -0700

    fix(tag-select): remove `MatchTag` type

--- components/tag-select/index.tsx
+++ components/tag-select/index.tsx
@@ -3,13 +3,12 @@ import useTranslation from 'next-translate/useTranslation';
 
 import SearchSelect, { SearchSelectProps } from 'components/search-select';
 
-import { DBMatchTag, MatchTag } from 'lib/model/match';
 import { DBMeetingTag, MeetingTag } from 'lib/model/meeting';
 import { DBUserTag, UserTag } from 'lib/model/user';
 
 export type TagSelectProps<
-  HitTag extends DBUserTag | DBMatchTag | DBMeetingTag,
-  Tag extends UserTag | MatchTag | MeetingTag
+  HitTag extends DBUserTag | DBMeetingTag,
+  Tag extends UserTag | MeetingTag
 > = Omit<
   SearchSelectProps<HitTag>,
   'index' | 'noResultsMessage' | 'options' | 'ref'
@@ -19,13 +18,13 @@ export type TagSelectProps<
 // component is going to be left un-optimized for now.
 // @see {@link https://github.com/facebook/react/issues/21003}
 export default function TagSelect<
-  HitTag extends DBUserTag | DBMatchTag | DBMeetingTag,
-  Tag extends UserTag | MatchTag | MeetingTag
+  HitTag extends DBUserTag | DBMeetingTag,
+  Tag extends UserTag | MeetingTag
 >({ options, ...props }: TagSelectProps<HitTag, Tag>): JSX.Element {
   const { t } = useTranslation();
 
   // TODO: TypeScript should know that if HitTag is DBUserTag then Tag has to
-  // be UserTag (and cannot be MatchTag or MeetingTag) and vice versa.
+  // be UserTag (and cannot be a MeetingTag) and vice versa.
   const selectOptions = useMemo(
     () => [
       ...((options as unknown) as HitTag[]),
