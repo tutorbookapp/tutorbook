afd34c03307a1a74b7409162958f7e7033ee78ad
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-06 22:03:02 -0700

    fix(api/verify/query-id): use `Number` str wrapper

--- lib/api/verify/query-id.ts
+++ lib/api/verify/query-id.ts
@@ -1,11 +1,11 @@
 import { APIError } from 'lib/model/error';
 
 export function verifyQueryIdNum(query: Record<string, unknown>): number {
-  if (typeof query.id !== 'number') {
+  if (Number.isNaN(Number(query.id))) {
     const msg = 'Expected query to contain an ID param with type number';
     throw new APIError(msg, 400);
   }
-  return query.id;
+  return Number(query.id);
 }
 
 export function verifyQueryId(query: Record<string, unknown>): string {
