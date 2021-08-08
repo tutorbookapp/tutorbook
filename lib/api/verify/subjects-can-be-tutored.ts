548badc209493a93ba33c2414b9cb240cee7c9ed
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-08 16:37:41 -0700

    docs(api/verify): remove outdated jsdoc comments

--- lib/api/verify/subjects-can-be-tutored.ts
+++ lib/api/verify/subjects-can-be-tutored.ts
@@ -1,16 +1,6 @@
 import { APIError } from 'lib/model/error';
 import { User } from 'lib/model/user';
 
-/**
- * Verifies that the given subjects can be tutored/mentored by all the mentors
- * and tutors included in the given people.
- * @param subjects - The subjects to verify.
- * @param people - The people that should be able to tutor/mentor the given
- * subjects (depending on their role).
- * @return Nothing; throws an `APIError` if a tutor or mentor (included in the
- * given `people`). isn't able to tutor/mentor the given `subjects`.
- * @todo Rename this function so it doesn't seem exclusive for tutoring.
- */
 export default function verifySubjectsCanBeTutored(
   subjects: string[],
   people: User[]
