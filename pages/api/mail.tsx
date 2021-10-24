5071ab15a646628a51ad76b5e2ece666f416bc01
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-10-24 14:37:01 -0700

    fix(pages/api/mail): address TypeScript errors

--- pages/api/mail.tsx
+++ pages/api/mail.tsx
@@ -1,16 +1,14 @@
 import { NextApiRequest as Req, NextApiResponse as Res } from 'next';
-import { renderToStaticMarkup } from 'react-dom/server';
 
-import Mail from 'pages/meeting-mail';
+import Mail from 'pages/mail';
 
 import send from 'lib/mail/send';
 
 export default async function mail(req: Req, res: Res): Promise<void> {
   await send({
-    to: [{ name: 'Nicholas Chiang', email: 'nicholas.h.chiang@gmail.com' }],
-    cc: { name: 'Erik Lucatero', email: 'erik@example.com' },
-    subject: 'Erik Lucatero booked a Computer Science meeting',
-    html: renderToStaticMarkup(<Mail />),
+    to: [{ name: 'Nicholas Chiang', email: 'nicholas@tutorbook.org' }],
+    subject: 'Erik canceled your computer science meeting',
+    template: <Mail />,
   });
   res.status(200).end('Email Sent');
 }
