bc4a13bce0391aced6831ef17455a6befcfc2b21
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-10-24 14:48:05 -0700

    fix(mail/meetings/update): remove "canceled" subject

--- lib/mail/meetings/update.tsx
+++ lib/mail/meetings/update.tsx
@@ -9,7 +9,7 @@ export default function mail(meeting: Meeting, updater: User): Promise<void> {
   return send({
     to,
     cc: updater,
-    subject: `${updater.firstName} canceled a meeting with you`,
+    subject: `${updater.firstName} updated a meeting with you`,
     template: (
       <Message>
         <P style={{ marginTop: '0' }}>Hi {join(to.map((p) => p.firstName))},</P>
