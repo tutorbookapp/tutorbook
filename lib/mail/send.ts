eeeaedf503b6a31be496fe0f8f21fa4a353db098
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-10-31 21:55:10 -0700

    feat(mail/send): replace SendGrid with Postmark

--- lib/mail/send.ts
+++ lib/mail/send.ts
@@ -1,32 +1,33 @@
-import { MailData } from '@sendgrid/helpers/classes/mail';
 import { ReactElement } from 'react';
-import mail from '@sendgrid/mail';
+import postmark from 'postmark';
 import { renderToStaticMarkup } from 'react-dom/server';
 import to from 'await-to-js';
 
 import { APIError } from 'lib/model/error';
 
-export type Email = {
+export interface Email {
+  subject: string;
   template: ReactElement;
-  replyTo?: { name?: string; email: string };
-  from?: { name?: string; email: string };
   to: { name?: string; email: string }[];
-} & Omit<MailData, 'replyTo' | 'from' | 'to'>;
+  cc?: { name?: string; email: string }[];
+  replyTo?: { name?: string; email: string };
+}
 
 export default async function send(email: Email): Promise<void> {
   if (['development', 'test'].includes(process.env.APP_ENV as string)) return;
-  if (typeof process.env.SENDGRID_API_KEY !== 'string') {
-    throw new APIError('Cannot send emails without SendGrid API key.');
+  if (typeof process.env.POSTMARK_API_KEY !== 'string') {
+    throw new APIError('Cannot send emails without Postmark API key.');
   } else {
-    mail.setApiKey(process.env.SENDGRID_API_KEY);
+    const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
     const [e] = await to(
-      mail.send({
-        ...email,
-        from: { name: 'Tutorbook', email: 'team@tutorbook.org' },
-        bcc: { name: 'Tutorbook', email: 'team@tutorbook.org' },
-        replyTo: email.replyTo?.email ? email.replyTo : undefined,
-        html: renderToStaticMarkup(email.template),
-        to: email.to.filter((p) => p.email),
+      client.sendEmail({
+        From: 'team@tutorbook.org',
+        Bcc: 'team@tutorbook.org',
+        To: email.to.map((u) => u.email).join(', '),
+        Cc: email.cc?.map((u) => u.email).join(', '),
+        ReplyTo: email.replyTo?.email,
+        Subject: email.subject,
+        HtmlBody: renderToStaticMarkup(email.template),
       })
     );
     if (e) throw new APIError(`${e.name} sending email: ${e.message}`, 500);
