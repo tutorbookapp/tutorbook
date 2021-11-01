4f4c1ebcddf11949bc07f3d657ae2dd261b1ce9b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-10-31 17:44:00 -0700

    feat(analytics): show descriptions while loading

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -35,7 +35,6 @@ interface CardProps<T> {
   header?: string;
   content?: { dataKey: string; dataLabel: string; rate?: boolean }[];
   color?: string;
-  loading?: boolean;
 }
 
 function Card<T extends Record<string, number> & { week: number }>({
@@ -45,7 +44,6 @@ function Card<T extends Record<string, number> & { week: number }>({
   header,
   content,
   color,
-  loading,
 }: CardProps<T>): JSX.Element {
   const num = useMemo(() => {
     if (!data || !content) return undefined;
@@ -57,13 +55,17 @@ function Card<T extends Record<string, number> & { week: number }>({
     <div className='card'>
       <article className='header'>
         <header>
-          <h2 className={cn({ loading })}>{content && content[0].rate && num ? formatRate(num) : num}</h2>
-          <h3 className={cn({ loading })}>{title}</h3>
+          <h2 className={cn({ loading: num === undefined })}>
+            {content && content[0].rate && num !== undefined ? formatRate(num) : num}
+          </h2>
+          <h3>{title}</h3>
         </header>
-        <p className={cn({ loading })}>{children}</p>
+        <p>{children}</p>
       </article>
-      {loading && <div className='graph loading' />}
-      {!loading && data && header && content && color && (
+      {!(data && header && content && color) && (
+        <div className='graph loading' />
+      )}
+      {data && header && content && color && (
         <Graph data={data} header={header} content={content} color={color} />
       )}
       <style jsx>{`
@@ -102,7 +104,7 @@ function Card<T extends Record<string, number> & { week: number }>({
         }
 
         h2.loading {
-          width: 60px;
+          width: 100px;
         }
 
         .header h3 {
@@ -113,19 +115,11 @@ function Card<T extends Record<string, number> & { week: number }>({
           height: 40px;
         }
 
-        h3.loading {
-          width: 150px;
-        }
-
         .header p {
           margin: 8px 0;
           font-size: 14px;
           font-weight: 400;
           color: var(--accents-5);
-        }
-
-        p.loading {
-          height: 52.5px;
         }
 
         .loading {
@@ -207,274 +201,267 @@ export default function Analytics(): JSX.Element {
   // @see {@link http://recharts.org/en-US/api/XAxis#scale}
   return (
     <main>
-      {!(usersWithMeetings && meetings && serviceHours && users) && (
-        <div className='graphs'>
-          {Array(10).fill(null).map((_, idx) => <Card key={idx} loading />)}
-        </div>
-      )}
-      {usersWithMeetings && meetings && serviceHours && users && (
-        <div className='graphs'>
-          <Card
-            title={
-              <>
-                Users with meetings
-                <br />
-                this week
-              </>
-            }
-            data={usersWithMeetings}
-            header='Users with meetings'
-            content={[
-              { dataKey: 'users', dataLabel: 'users with meetings' },
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-            ]}
-            color='#64B5F6'
-          >
-            The number of users who have had a meeting in the last week. This is
-            your{' '}
-            <Link href='https://thoughtbot.com/blog/north-star-metric'>
-              north star metric
-            </Link>
-            ; it measures how much authentic value you are providing to your
-            community.
-          </Card>
-          <Card
-            title={
-              <>
-                Weekly growth rate of
-                <br />
-                users with meetings
-              </>
-            }
-            data={usersWithMeetings}
-            header='Users with meetings'
-            content={[
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-              { dataKey: 'users', dataLabel: 'users with meetings' },
-            ]}
-            color='#64B5F6'
-          >
-            The growth rate of the number of users who have had a meeting in the
-            last week. You should always focus on growth rates instead of totals
-            to succeed; a growth rate of{' '}
-            <Link href='http://www.paulgraham.com/growth.html'>
-              5-7% per week
-            </Link>{' '}
-            is good.
-          </Card>
-          <Card
-            title={
-              <>
-                Meetings
-                <br />
-                this week
-              </>
-            }
-            data={meetings}
-            header='Meetings'
-            content={[
-              { dataKey: 'meetings', dataLabel: 'meetings per week' },
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-            ]}
-            color='#81C784'
-          >
-            The number of meetings per week. This is another{' '}
-            <Link href='https://kpi.org/KPI-Basics'>
-              key performance indicator
-            </Link>{' '}
-            that is directly correlated with your{' '}
-            <Link href='https://thoughtbot.com/blog/north-star-metric'>
-              north star metric
-            </Link>
-            : the number of users with meetings per week.
-          </Card>
-          <Card
-            title={
-              <>
-                Weekly growth rate of
-                <br />
-                meetings per week
-              </>
-            }
-            data={meetings}
-            header='Meetings'
-            content={[
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-              { dataKey: 'meetings', dataLabel: 'meetings per week' },
-            ]}
-            color='#81C784'
-          >
-            The growth rate of the number of meetings per week. This graph
-            should look very similar to the growth rate of users with meetings
-            per week; both metrics are directly correlated.
-          </Card>
-          <Card
-            title={
-              <>
-                Service hours
-                <br />
-                this week
-              </>
-            }
-            data={serviceHours}
-            header='Service hours'
-            content={[
-              { dataKey: 'hours', dataLabel: 'service hours per week' },
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-            ]}
-            color='#FF8A65'
-          >
-            The number of service hours tracked per week.
-          </Card>
-          <Card
-            title={
-              <>
-                Weekly growth rate of
-                <br />
-                service hours per week
-              </>
-            }
-            data={serviceHours}
-            header='Service hours'
-            content={[
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-              { dataKey: 'hours', dataLabel: 'service hours per week' },
-            ]}
-            color='#FF8A65'
-          >
-            The growth rate of the number of service hours tracked per week.
-          </Card>
-          <Card
-            title={
-              <>
-                New users
-                <br />
-                this week
-              </>
-            }
-            data={users}
-            header='New users'
-            content={[
-              { dataKey: 'users', dataLabel: 'new users' },
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-            ]}
-            color='#F06292'
-          >
-            The number of users created per week. You’ll notice this graph
-            correlates well with the <strong>Total users</strong> graph below
-            it; as the number of new users spikes, the total number of users
-            will spike too.
-          </Card>
-          <Card
-            title={
-              <>
-                Weekly growth rate of
-                <br />
-                new users per week
-              </>
-            }
-            data={users}
-            header='New users'
-            content={[
-              {
-                dataKey: 'growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-              { dataKey: 'users', dataLabel: 'new users' },
-            ]}
-            color='#F06292'
-          >
-            The growth rate of the number of users created per week. Note that
-            all of these growth rate graphs depict the derivatives of their
-            corresponding weekly metrics; they are graphs of the slopes.
-          </Card>
-          <Card
-            title={
-              <>
-                Users
-                <br />
-                in all time
-              </>
-            }
-            data={users}
-            header='Total users'
-            content={[
-              { dataKey: 'total', dataLabel: 'total users' },
-              {
-                dataKey: 'total_growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-            ]}
-            color='#9575CD'
-          >
-            The total number of users. This is a{' '}
-            <Link href='https://hbr.org/2010/02/entrepreneurs-beware-of-vanity-metrics'>
-              vanity metric
-            </Link>
-            ; a number that looks good on paper but isn’t action oriented. Use
-            it for press releases or marketing, but not to measure actual
-            growth.
-          </Card>
-          <Card
-            title={
-              <>
-                Weekly growth rate of
-                <br />
-                the total number of users
-              </>
-            }
-            data={users}
-            header='Total users'
-            content={[
-              {
-                dataKey: 'total_growth',
-                dataLabel: 'from previous week',
-                rate: true,
-              },
-              { dataKey: 'total', dataLabel: 'total users' },
-            ]}
-            color='#9575CD'
-          >
-            The weekly growth rate of the total number of users. Again, this is
-            a{' '}
-            <Link href='https://hbr.org/2010/02/entrepreneurs-beware-of-vanity-metrics'>
-              vanity metric
-            </Link>
-            ; this growth rate will <i>always</i> be positive and thus will{' '}
-            <i>never</i> provide meaningful feedback on how growth is doing.
-          </Card>
-        </div>
-      )}
+      <div className='graphs'>
+        <Card
+          title={
+            <>
+              Users with meetings
+              <br />
+              this week
+            </>
+          }
+          data={usersWithMeetings}
+          header='Users with meetings'
+          content={[
+            { dataKey: 'users', dataLabel: 'users with meetings' },
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+          ]}
+          color='#64B5F6'
+        >
+          The number of users who have had a meeting in the last week. This is
+          your{' '}
+          <Link href='https://thoughtbot.com/blog/north-star-metric'>
+            north star metric
+          </Link>
+          ; it measures how much authentic value you are providing to your
+          community.
+        </Card>
+        <Card
+          title={
+            <>
+              Weekly growth rate of
+              <br />
+              users with meetings
+            </>
+          }
+          data={usersWithMeetings}
+          header='Users with meetings'
+          content={[
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+            { dataKey: 'users', dataLabel: 'users with meetings' },
+          ]}
+          color='#64B5F6'
+        >
+          The growth rate of the number of users who have had a meeting in the
+          last week. You should always focus on growth rates instead of totals
+          to succeed; a growth rate of{' '}
+          <Link href='http://www.paulgraham.com/growth.html'>
+            5-7% per week
+          </Link>{' '}
+          is good.
+        </Card>
+        <Card
+          title={
+            <>
+              Meetings
+              <br />
+              this week
+            </>
+          }
+          data={meetings}
+          header='Meetings'
+          content={[
+            { dataKey: 'meetings', dataLabel: 'meetings per week' },
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+          ]}
+          color='#81C784'
+        >
+          The number of meetings per week. This is another{' '}
+          <Link href='https://kpi.org/KPI-Basics'>
+            key performance indicator
+          </Link>{' '}
+          that is directly correlated with your{' '}
+          <Link href='https://thoughtbot.com/blog/north-star-metric'>
+            north star metric
+          </Link>
+          : the number of users with meetings per week.
+        </Card>
+        <Card
+          title={
+            <>
+              Weekly growth rate of
+              <br />
+              meetings per week
+            </>
+          }
+          data={meetings}
+          header='Meetings'
+          content={[
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+            { dataKey: 'meetings', dataLabel: 'meetings per week' },
+          ]}
+          color='#81C784'
+        >
+          The growth rate of the number of meetings per week. This graph
+          should look very similar to the growth rate of users with meetings
+          per week; both metrics are directly correlated.
+        </Card>
+        <Card
+          title={
+            <>
+              Service hours
+              <br />
+              this week
+            </>
+          }
+          data={serviceHours}
+          header='Service hours'
+          content={[
+            { dataKey: 'hours', dataLabel: 'service hours per week' },
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+          ]}
+          color='#FF8A65'
+        >
+          The number of service hours tracked per week.
+        </Card>
+        <Card
+          title={
+            <>
+              Weekly growth rate of
+              <br />
+              service hours per week
+            </>
+          }
+          data={serviceHours}
+          header='Service hours'
+          content={[
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+            { dataKey: 'hours', dataLabel: 'service hours per week' },
+          ]}
+          color='#FF8A65'
+        >
+          The growth rate of the number of service hours tracked per week.
+        </Card>
+        <Card
+          title={
+            <>
+              New users
+              <br />
+              this week
+            </>
+          }
+          data={users}
+          header='New users'
+          content={[
+            { dataKey: 'users', dataLabel: 'new users' },
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+          ]}
+          color='#F06292'
+        >
+          The number of users created per week. You’ll notice this graph
+          correlates well with the <strong>Total users</strong> graph below
+          it; as the number of new users spikes, the total number of users
+          will spike too.
+        </Card>
+        <Card
+          title={
+            <>
+              Weekly growth rate of
+              <br />
+              new users per week
+            </>
+          }
+          data={users}
+          header='New users'
+          content={[
+            {
+              dataKey: 'growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+            { dataKey: 'users', dataLabel: 'new users' },
+          ]}
+          color='#F06292'
+        >
+          The growth rate of the number of users created per week. Note that
+          all of these growth rate graphs depict the derivatives of their
+          corresponding weekly metrics; they are graphs of the slopes.
+        </Card>
+        <Card
+          title={
+            <>
+              Users
+              <br />
+              in all time
+            </>
+          }
+          data={users}
+          header='Total users'
+          content={[
+            { dataKey: 'total', dataLabel: 'total users' },
+            {
+              dataKey: 'total_growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+          ]}
+          color='#9575CD'
+        >
+          The total number of users. This is a{' '}
+          <Link href='https://hbr.org/2010/02/entrepreneurs-beware-of-vanity-metrics'>
+            vanity metric
+          </Link>
+          ; a number that looks good on paper but isn’t action oriented. Use
+          it for press releases or marketing, but not to measure actual
+          growth.
+        </Card>
+        <Card
+          title={
+            <>
+              Weekly growth rate of
+              <br />
+              the total number of users
+            </>
+          }
+          data={users}
+          header='Total users'
+          content={[
+            {
+              dataKey: 'total_growth',
+              dataLabel: 'from previous week',
+              rate: true,
+            },
+            { dataKey: 'total', dataLabel: 'total users' },
+          ]}
+          color='#9575CD'
+        >
+          The weekly growth rate of the total number of users. Again, this is
+          a{' '}
+          <Link href='https://hbr.org/2010/02/entrepreneurs-beware-of-vanity-metrics'>
+            vanity metric
+          </Link>
+          ; this growth rate will <i>always</i> be positive and thus will{' '}
+          <i>never</i> provide meaningful feedback on how growth is doing.
+        </Card>
+      </div>
       <style jsx>{`
         main {
           max-width: var(--page-width-with-margin);
