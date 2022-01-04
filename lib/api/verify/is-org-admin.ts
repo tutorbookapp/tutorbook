7579c5a62d2d1344ffe6664ac5a5415738705cf6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-01-04 12:33:19 -0800

    wip(api/verify/is-org-admin): send descriptive error

--- lib/api/verify/is-org-admin.ts
+++ lib/api/verify/is-org-admin.ts
@@ -3,7 +3,7 @@ import { Org } from 'lib/model/org';
 
 export default function verifyIsOrgAdmin(org: Org, uid: string): void {
   if (!org.members.includes(uid)) {
-    const msg = `User (${uid}) is not a ${org.toString()} admin`;
+    const msg = `You are not a ${org.name} admin. To fix, make sure to select yourself as a "Parent" above and try again.`;
     throw new APIError(msg, 400);
   }
 }
